f073a11ce95b8d03c571f82762998c3a551d13b4
Atralupus <me@atralupus.com>
2024-02-21 22:03:21 +0900

    Init

---
+++ NineChroniclesUtilBackend.Store/Program.cs
@@ -0,0 +1,7 @@
+using NineChroniclesUtilBackend.Store;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
