5faf417925520e4881c1a1dfa3c26a521edefa31
Atralupus <me@atralupus.com>
2024-02-22 11:43:54 +0900

    Add big integer converter

---
+++ NineChroniclesUtilBackend.Store/Util/JsonConverter.cs
@@ -0,0 +1,20 @@
+using System.Numerics;
+using Newtonsoft.Json;
+
+public class BigIntegerToStringConverter : JsonConverter
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(BigInteger);
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        writer.WriteValue(value.ToString());
+    }
+}
