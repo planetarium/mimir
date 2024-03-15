7b05dc694b52af70f0d29e352a1d4472b7ca48b6
Atralupus <me@atralupus.com>
2024-03-16 02:32:28 +0900

    Handle collection data

--- NineChroniclesUtilBackend/Repositories/CpRepository.cs
+++ NineChroniclesUtilBackend/Repositories/CpRepository.cs
@@ -1,23 +1,23 @@
-using System.Text.Json.Nodes;
-using Bencodex;
 using Bencodex.Types;
 using Libplanet.Crypto;
-using Nekoyume;
 using Nekoyume.Battle;
 using Nekoyume.Model.Item;
+using Nekoyume.Model.Stat;
 using Nekoyume.Model.State;
 using Nekoyume.TableData;
 using NineChroniclesUtilBackend.Models.Agent;
+using NineChroniclesUtilBackend.Services;
+using NineChroniclesUtilBackend.Util;
 
 namespace NineChroniclesUtilBackend.Repositories;
 
 public class CpRepository
 {
-    private Uri emptyChronicleEndpoint { get; set; }
+    private StateGetter _stateGetter;
 
-    public CpRepository(Uri _emptyChronicleEndpoint)
+    public CpRepository(IStateService stateService)
     {
-        emptyChronicleEndpoint = _emptyChronicleEndpoint;
+        _stateGetter = new StateGetter(stateService);
     }
 
     public async Task<int?> CalculateCp(
@@ -30,20 +30,28 @@ public class CpRepository
     {
         try
         {
-            var inventory = await GetInventory(avatar.AvatarAddress);
-            var equipments = equipmentIds
-                .Select(x => inventory.GetValueOrDefault(x))
-                .Where(x => x != null)
-                .Select(x => new Equipment(x))
-                .Where(x => x.Equipped);
-            var costumes = costumeIds
-                .Select(x => inventory.GetValueOrDefault(x))
-                .Where(x => x != null)
-                .Select(x => new Costume(x))
-                .Where(x => x.Equipped);
-            var runes = runeOption.Select(x => GetRuneOptionInfo(x.id, x.level).Result);
+            var avatarAddress = new Address(avatar.AvatarAddress);
+
             var characterRow = await GetCharacterRow(characterId);
-            var costumeStatSheet = await GetSheet(new CostumeStatSheet());
+            var costumeStatSheet = await _stateGetter.GetSheet<CostumeStatSheet>();
+            var collectionSheets = await _stateGetter.GetSheet<CollectionSheet>();
+
+            var inventoryState = await _stateGetter.GetInventoryState(avatarAddress);
+
+            List<Equipment> equipments = inventoryState
+                .Equipments
+                .Where(x => x.Equipped).ToList();
+            List<Costume> costumes = inventoryState
+                .Costumes
+                .Where(x => x.Equipped).ToList();
+            List<RuneOptionSheet.Row.RuneOptionInfo> runes = runeOption.Select(x => GetRuneOptionInfo(x.id, x.level).Result).ToList();
+            var collectionState = await _stateGetter.GetCollectionStates([avatarAddress]);
+
+            List<StatModifier> modifier = [];
+            foreach (var collectionId in collectionState[avatarAddress].Ids)
+            {
+                modifier.AddRange(collectionSheets[collectionId].StatModifiers);
+            }
 
             return CPHelper.TotalCP(
                 equipments,
@@ -51,7 +59,8 @@ public class CpRepository
                 runes,
                 avatar.Level,
                 characterRow,
-                costumeStatSheet
+                costumeStatSheet,
+                modifier
             );
         }
         catch (NullReferenceException)
@@ -60,31 +69,12 @@ public class CpRepository
         }
     }
 
-    private async Task<Dictionary<string, Dictionary>> GetInventory(string address)
-    {
-        var inventoryState = await GetInventoryState(address);
-
-        return inventoryState
-            .Select(x => (Dictionary)((Dictionary)x)["item"])
-            .Where(x =>
-                ((Text)x["item_type"]).Value == "Costume"
-                || ((Text)x["item_type"]).Value == "Equipment"
-            )
-            .ToDictionary(x =>
-            {
-                if (((Text)x["item_type"]).Value == "Costume")
-                    return ((Binary)x["item_id"]).ToGuid().ToString();
-
-                return ((Binary)x["itemId"]).ToGuid().ToString();
-            });
-    }
-
     private async Task<RuneOptionSheet.Row.RuneOptionInfo> GetRuneOptionInfo(
         int id,
         int level
     )
     {
-        var sheets = await GetSheet(new RuneOptionSheet());
+        var sheets = await _stateGetter.GetSheet<RuneOptionSheet>();
 
         if (!sheets.TryGetValue(id, out var optionRow))
         {
@@ -101,7 +91,7 @@ public class CpRepository
 
     private async Task<CharacterSheet.Row> GetCharacterRow(int characterId)
     {
-        var sheets = await GetSheet(new CharacterSheet());
+        var sheets = await _stateGetter.GetSheet<CharacterSheet>();
 
         if (!sheets.TryGetValue(characterId, out var row))
         {
@@ -110,42 +100,4 @@ public class CpRepository
 
         return row;
     }
-
-    private async Task<T> GetSheet<T>(T sheets)
-        where T : ISheet
-    {
-        var sheetsAddress = Addresses.GetSheetAddress<T>();
-        var rawSheets = await GetState<Text>(sheetsAddress);
-        sheets.Set(rawSheets);
-
-        return sheets;
-    }
-
-    private async Task<T> GetState<T>(Address address, string? account = null)
-        where T : IValue => await GetState<T>(address.ToString(), account);
-
-    // TODO: Cache
-    private async Task<T?> GetState<T>(string address, string? account = null)
-        where T : IValue
-    {
-        var codec = new Codec();
-        var url =
-            account == null
-                ? $"{emptyChronicleEndpoint.AbsoluteUri}api/states/{address}/raw"
-                : $"{emptyChronicleEndpoint.AbsoluteUri}api/states/{address}/raw?account={account}";
-        var client = new HttpClient();
-        var response = await client.GetAsync(url);
-        var json = JsonNode.Parse(await response.Content.ReadAsStringAsync());
-        var rawValue = json["value"]?.GetValue<string>();
-        if (rawValue == null || rawValue == "")
-        {
-            throw new NullReferenceException();
-        }
-        var value = codec.Decode(Convert.FromHexString(rawValue));
-
-        return (T)value;
-    }
-
-    private async Task<List> GetInventoryState(string address) =>
-        await GetState<List?>(address, Addresses.Inventory.ToString()) ?? [];
 }
