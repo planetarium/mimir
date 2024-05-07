a5afe5e5fba0326ab015c651c43dfa9084f714a4
Hyun Seungmin <boscohyun@gmail.com>
2024-05-07 16:43:53 +0900

    handle 404 of avatars/{avatarAddress}/inventory

--- NineChroniclesUtilBackend/Controllers/AvatarController.cs
+++ NineChroniclesUtilBackend/Controllers/AvatarController.cs
@@ -9,6 +9,15 @@ namespace NineChroniclesUtilBackend.Controllers;
 public class AvatarController(AvatarRepository avatarRepository) : ControllerBase
 {
     [HttpGet("{avatarAddress}/inventory")]
-    public Inventory GetInventory(string avatarAddress) =>
-        avatarRepository.GetInventory(avatarAddress);
+    public Inventory? GetInventory(string avatarAddress)
+    {
+        var inventory = avatarRepository.GetInventory(avatarAddress);
+        if (inventory is null)
+        {
+            Response.StatusCode = StatusCodes.Status404NotFound;
+            return null;
+        }
+
+        return inventory;
+    }
 }
--- NineChroniclesUtilBackend/Repositories/AvatarRepository.cs
+++ NineChroniclesUtilBackend/Repositories/AvatarRepository.cs
@@ -10,11 +10,16 @@ public class AvatarRepository(MongoDBCollectionService mongoDBCollectionService)
     private readonly IMongoCollection<BsonDocument> _avatarsCollection =
         mongoDBCollectionService.GetCollection<BsonDocument>("avatars");
 
-    public Inventory GetInventory(string avatarAddress)
+    public Inventory? GetInventory(string avatarAddress)
     {
         var filter = Builders<BsonDocument>.Filter.Eq(f => f["Avatar"]["address"], avatarAddress);
         var projection = Builders<BsonDocument>.Projection.Include(f => f["Avatar"]["inventory"]["Equipments"]);
         var document = _avatarsCollection.Find(filter).Project(projection).FirstOrDefault();
+        if (document is null)
+        {
+            return null;
+        }
+        
         return new Inventory(document["Avatar"]["inventory"]);
     }
 }
