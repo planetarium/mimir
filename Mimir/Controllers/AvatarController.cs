0d9d0c39e5b83ca476536b8838c67ab2c5b173fe
Hyun Seungmin <boscohyun@gmail.com>
2024-05-13 12:17:44 +0900

    add temporary snippets  to AvatarController

--- Mimir/Controllers/AvatarController.cs
+++ Mimir/Controllers/AvatarController.cs
@@ -8,6 +8,87 @@ namespace Mimir.Controllers;
 [Route("{network}/avatars")]
 public class AvatarController(AvatarRepository avatarRepository) : ControllerBase
 {
+    #region temporary snippets
+
+    // This is a snippet from Mimir/Util/BigIntegerToStringConverter.cs
+    private class BigIntegerToStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(BigInteger);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object? existingValue,
+            JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value?.ToString());
+        }
+    }
+
+    // This is a snippet from Mimir/Util/StateJsonConverter.cs
+    private class StateJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(IState).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(
+            JsonReader reader,
+            Type objectType,
+            object? existingValue,
+            JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            if (value is null)
+            {
+                return;
+            }
+
+            var jo = JObject.FromObject(value, JsonSerializer.CreateDefault(new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                Converters = new List<JsonConverter> { new BigIntegerToStringConverter() },
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore
+            }));
+
+            var iValue = value switch
+            {
+                AvatarState avatarState => avatarState.SerializeList(),
+                IState state => state.Serialize(),
+                _ => null
+            };
+
+            if (iValue != null)
+            {
+                var rawValue = ByteUtil.Hex(new Codec().Encode(iValue));
+                jo.Add("Raw", rawValue);
+            }
+
+            jo.WriteTo(writer);
+        }
+    }
+
+    // This is a snippet from Mimir.Worker/Models/State/BaseData.cs
+    private static JsonSerializerSettings JsonSerializerSettings => new()
+    {
+        Converters = { new StateJsonConverter() },
+        Formatting = Formatting.Indented,
+        NullValueHandling = NullValueHandling.Ignore
+    };
+
+    #endregion snippets
+
     [HttpGet("{avatarAddress}/inventory")]
     public Inventory? GetInventory(string network, string avatarAddress)
     {
