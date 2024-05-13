e326212b8151fa4704364b27d80db8e2b7b5251d
Hyun Seungmin <boscohyun@gmail.com>
2024-05-13 18:44:03 +0900

    implement overloaded constructor for a sevaral models

--- Mimir/Models/Items/Equipment.cs
+++ Mimir/Models/Items/Equipment.cs
@@ -2,7 +2,17 @@ using MongoDB.Bson;
 
 namespace Mimir.Models.Items;
 
-public class Equipment(BsonValue equipment) : NonFungibleItem(equipment)
+public class Equipment : NonFungibleItem
 {
-    public int Level { get; set; } = equipment["level"].AsInt32;
+    public int Level { get; set; }
+
+    public Equipment(Nekoyume.Model.Item.Equipment equipment) : base(equipment)
+    {
+        Level = equipment.level;
+    }
+
+    public Equipment(BsonValue equipment) : base(equipment)
+    {
+        Level = equipment["level"].AsInt32;
+    }
 }
--- Mimir/Models/Items/NonFungibleItem.cs
+++ Mimir/Models/Items/NonFungibleItem.cs
@@ -1,11 +1,21 @@
 using MongoDB.Bson;
+using Nekoyume.Model.Item;
 
 namespace Mimir.Models.Items;
 
-public class NonFungibleItem(BsonValue nonFungibleItem) : Item(nonFungibleItem)
+public class NonFungibleItem : Item
 {
-    public Guid NonFungibleId { get; set; } =
-        Guid.TryParse(nonFungibleItem["NonFungibleId"].AsString, out var nonFungibleId)
+    public Guid NonFungibleId { get; set; }
+
+    public NonFungibleItem(INonFungibleItem nonFungibleItem) : base(nonFungibleItem)
+    {
+        NonFungibleId = nonFungibleItem.NonFungibleId;
+    }
+
+    public NonFungibleItem(BsonValue nonFungibleItem) : base(nonFungibleItem)
+    {
+        NonFungibleId = Guid.TryParse(nonFungibleItem["NonFungibleId"].AsString, out var nonFungibleId)
             ? nonFungibleId
             : Guid.Empty;
+    }
 }
