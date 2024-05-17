4120a17b86cde3380a87b2c830c04652a3dbb093
Hyun Seungmin <boscohyun@gmail.com>
2024-05-17 17:02:08 +0900

    introduce "{network}/agent/{address}/balances"

--- Mimir/Controllers/AgentController.cs
+++ Mimir/Controllers/AgentController.cs
@@ -1,4 +1,6 @@
+using Lib9c;
 using Libplanet.Crypto;
+using Libplanet.Types.Assets;
 using Microsoft.AspNetCore.Mvc;
 using Mimir.Models.Agent;
 using Mimir.Services;
@@ -7,26 +9,69 @@ using Mimir.Util;
 namespace Mimir.Controllers;
 
 [ApiController]
-[Route("{network}/agent")]
+[Route("{network}/agent/{address}")]
 public class AgentController : ControllerBase
 {
-    [HttpGet("{agentAddress}/avatars")]
+#pragma warning disable CS0618 // Type or member is obsolete
+    private readonly Currency _ncg = Currency.Legacy(
+        "NCG",
+        2,
+        new Address("0x47D082a115c63E7b58B1532d20E631538eaFADde"));
+#pragma warning restore CS0618 // Type or member is obsolete
+
+    [HttpGet("balances")]
+    public async Task<List<string>> GetBalances(
+        string network,
+        string address,
+        IStateService stateService)
+    {
+        Address agentAddress;
+        try
+        {
+            agentAddress = new Address(address);
+        }
+        catch (FormatException)
+        {
+            Response.StatusCode = StatusCodes.Status400BadRequest;
+            return [];
+        }
+
+        var stateGetter = new StateGetter(stateService);
+        var ncg = await stateGetter.GetBalanceAsync(agentAddress, _ncg);
+        var crystal = await stateGetter.GetBalanceAsync(agentAddress, Currencies.Crystal);
+        return
+        [
+            $"{ncg} {_ncg.Ticker}",
+            $"{crystal} {Currencies.Crystal.Ticker}",
+        ];
+    }
+
+    [HttpGet("avatars")]
     public async Task<AvatarsResponse> GetAvatars(
         string network,
-        string agentAddress,
+        string address,
         IStateService stateService
     )
     {
+        Address agentAddress;
+        try
+        {
+            agentAddress = new Address(address);
+        }
+        catch (FormatException)
+        {
+            Response.StatusCode = StatusCodes.Status400BadRequest;
+            return new AvatarsResponse([]);
+        }
+
         var stateGetter = new StateGetter(stateService);
-        var avatars = await stateGetter.GetAvatarStatesAsync(new Address(agentAddress));
+        var avatars = await stateGetter.GetAvatarStatesAsync(agentAddress);
         if (avatars is null)
         {
             Response.StatusCode = StatusCodes.Status404NotFound;
             return new AvatarsResponse([]);
         }
 
-        return new AvatarsResponse(
-            avatars.Select(e => new Avatar(e)).ToList()
-        );
+        return new AvatarsResponse(avatars.Select(e => new Avatar(e)).ToList());
     }
 }
