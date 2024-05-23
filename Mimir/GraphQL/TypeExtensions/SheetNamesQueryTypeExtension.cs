0a4c01ae1e9e35e1077edaf50204c7cf9aff39e2
Hyun Seungmin <boscohyun@gmail.com>
2024-05-23 22:55:50 +0900

    introduce "sheetNames" query

---
+++ Mimir/GraphQL/TypeExtensions/SheetNamesQueryTypeExtension.cs
@@ -0,0 +1,22 @@
+using Lib9c.GraphQL.Types;
+using Mimir.GraphQL.Queries;
+using Mimir.Repositories;
+
+namespace Mimir.GraphQL.TypeExtensions;
+
+public class SheetNamesQueryTypeExtension : ObjectTypeExtension<Query>
+{
+    protected override void Configure(IObjectTypeDescriptor<Query> descriptor)
+    {
+        descriptor
+            .Field("sheetNames")
+            .Argument("planetName", a => a.Type<NonNullType<PlanetNameType>>())
+            .Type<NonNullType<ListType<NonNullType<StringType>>>>()
+            .ResolveWith<SheetNamesQueryTypeExtension>(t => GetSheetNames(default!, default!));
+    }
+
+    private static string[] GetSheetNames(
+        string planetName,
+        [Service] TableSheetsRepository tableSheetsRepository) =>
+        tableSheetsRepository.GetSheetNames(planetName);
+}
