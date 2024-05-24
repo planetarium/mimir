cc1e40441224c2f1282a6a4f2740837f97bcd314
Hyun Seungmin <boscohyun@gmail.com>
2024-05-24 09:59:06 +0900

    clean up minor

--- Lib9c.GraphQL/Objects/SheetObject.cs
+++ Lib9c.GraphQL/Objects/SheetObject.cs
@@ -3,7 +3,6 @@ namespace Lib9c.GraphQL.Objects;
 public class SheetObject
 {
     public string Name { get; set; } = string.Empty;
-    // public Address Address { get; set; } = default;
     public string? Csv { get; set; }
     public string? Json { get; set; }
 }
--- Lib9c.GraphQL/Types/SheetType.cs
+++ Lib9c.GraphQL/Types/SheetType.cs
@@ -7,15 +7,18 @@ namespace Lib9c.GraphQL.Types
     {
         protected override void Configure(IObjectTypeDescriptor<SheetObject> descriptor)
         {
-            descriptor.Field(f => f.Name)
+            descriptor
+                .Field(f => f.Name)
                 .Name("name")
                 .Description("The name of the sheet.")
                 .Type<NonNullType<SheetNameType>>();
-            descriptor.Field(f => f.Csv)
+            descriptor
+                .Field(f => f.Csv)
                 .Name("csv")
                 .Description("The CSV content of the sheet.")
                 .Type<StringType>();
-            descriptor.Field(f => f.Json)
+            descriptor
+                .Field(f => f.Json)
                 .Name("json")
                 .Description("The JSON content of the sheet.")
                 .Type<StringType>();
