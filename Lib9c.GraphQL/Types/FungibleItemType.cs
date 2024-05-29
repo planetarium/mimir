a27f9e565b27944ad0dfb9fb00b2c214f78eca95
Hyun Seungmin <boscohyun@gmail.com>
2024-05-29 17:06:34 +0900

    add some interface type to Lib9c.GarphQL.Types

---
+++ Lib9c.GraphQL/Types/FungibleItemType.cs
@@ -0,0 +1,15 @@
+using HotChocolate.Types;
+using Nekoyume.Model.Item;
+
+namespace Lib9c.GraphQL.Types;
+
+public class FungibleItemType : InterfaceType<IFungibleItem>
+{
+    protected override void Configure(IInterfaceTypeDescriptor<IFungibleItem> descriptor)
+    {
+        descriptor
+            .Field(t => t.FungibleId)
+            .Description("The fungible ID of the item.")
+            .Type<NonNullType<HashDigestSHA256Type>>();
+    }
+}
