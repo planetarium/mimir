857fafe5e0cbe60593ba9bd7fb5be5928829b60d
Hyun Seungmin <boscohyun@gmail.com>
2024-05-30 12:15:52 +0900

    introduce "avatar" field to root query

--- Mimir/GraphQL/Types/QueryType.cs
+++ Mimir/GraphQL/Types/QueryType.cs
@@ -1,7 +1,26 @@
+using Lib9c.GraphQL.Enums;
+using Lib9c.GraphQL.Types;
+using Libplanet.Crypto;
+using Mimir.GraphQL.Objects;
 using Mimir.GraphQL.Queries;
 
 namespace Mimir.GraphQL.Types;
 
 public class QueryType : ObjectType<Query>
 {
+    protected override void Configure(IObjectTypeDescriptor<Query> descriptor)
+    {
+        descriptor
+            .Field("avatar")
+            .Argument("planetName", a => a.Type<NonNullType<PlanetNameEnumType>>())
+            .Argument("address", a => a.Type<NonNullType<AddressType>>())
+            .Type<AvatarType>()
+            .Resolve(context =>
+            {
+                context.ScopedContextData = context.ScopedContextData
+                    .Add("planetName", context.ArgumentValue<PlanetName>("planetName"))
+                    .Add("avatarAddress", context.ArgumentValue<Address>("address"));
+                return new AvatarObject();
+            });
+    }
 }
