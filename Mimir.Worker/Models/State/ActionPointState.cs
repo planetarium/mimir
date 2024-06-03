c72b00465d72eb4ca0f3e5b3533d4696416e63c6
Hyun Seungmin <boscohyun@gmail.com>
2024-06-03 11:34:50 +0900

    introduce ActionPointState with handler

---
+++ Mimir.Worker/Models/State/ActionPointState.cs
@@ -0,0 +1,9 @@
+using Libplanet.Crypto;
+using Nekoyume.Model.State;
+
+namespace Mimir.Worker.Models;
+
+public class ActionPointState(Address address, int value) : State(address)
+{
+    public int Value = value;
+}
