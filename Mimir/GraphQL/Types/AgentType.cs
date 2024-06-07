3d3533a076cff906bf2b1ce64ac626b04618f3b9
Hyun Seungmin <boscohyun@gmail.com>
2024-06-07 16:27:19 +0900

    expand "agent" graphql query fields

--- Mimir/GraphQL/Types/AgentType.cs
+++ Mimir/GraphQL/Types/AgentType.cs
@@ -1,6 +1,10 @@
+using HotChocolate.Resolvers;
+using Lib9c.GraphQL.Enums;
 using Lib9c.GraphQL.Types;
 using Libplanet.Crypto;
 using Mimir.GraphQL.Objects;
+using Mimir.Models;
+using Mimir.Repositories;
 using Nekoyume;
 
 namespace Mimir.GraphQL.Types;
@@ -10,8 +14,20 @@ public class AgentType : ObjectType<AgentObject>
     protected override void Configure(IObjectTypeDescriptor<AgentObject> descriptor)
     {
         descriptor
+            .Field("version")
+            .Type<IntType>()
+            .Resolve(context => GetAgent(context)?.Version ?? null);
+        descriptor
             .Field(f => f.Address)
             .Type<NonNullType<AddressType>>();
+        descriptor
+            .Field("avatarAddresses")
+            .Type<NonNullType<ListType<NonNullType<AddressType>>>>()
+            .Resolve(context => GetAgent(context)?.AvatarAddresses.Values.ToArray() ?? []);
+        descriptor
+            .Field("monsterCollectionRound")
+            .Type<IntType>()
+            .Resolve(context => GetAgent(context)?.MonsterCollectionRound ?? null);
         descriptor
             .Field("avatar")
             .Argument("index", a => a.Type<NonNullType<IntType>>())
@@ -46,5 +62,55 @@ public class AgentType : ObjectType<AgentObject>
                         tuple.index))
                     .ToList();
             });
+    }
+
+    private static (
+        AgentRepository agentRepo,
+        PlanetName planetName,
+        Address agentAddress)? GetSource(IResolverContext context)
+    {
+        var agentRepo = context.Services.GetService<AgentRepository>();
+        if (agentRepo is null)
+        {
+            return null;
+        }
+
+        var planetName = context.ScopedContextData.TryGetValue("planetName", out var pn)
+            ? (PlanetName?)pn
+            : null;
+        if (planetName is null)
+        {
+            return null;
+        }
+
+        var agentAddress = context.Parent<AgentObject>().Address;
+        return (agentRepo, planetName.Value, agentAddress);
+    }
+
+    private static Agent? GetAgent(IResolverContext context)
+    {
+        var agent = context.ScopedContextData.TryGetValue("agent", out var a)
+            ? (Agent?)a
+            : null;
+        if (agent is not null)
+        {
+            return agent;
+        }
+
+        var tuple = GetSource(context);
+        if (tuple is null)
+        {
+            return null;
+        }
+
+        var (agentRepo, planetName, agentAddress) = tuple.Value;
+        agent = agentRepo.GetAgent(planetName, agentAddress);
+        if (agent is null)
+        {
+            return null;
+        }
+
+        context.ScopedContextData = context.ScopedContextData.Add("agent", agent);
+        return agent;
     }
 }
