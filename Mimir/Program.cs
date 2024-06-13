d4d5658932402089bc735c6a8483f5d04721c9a1
Hyun Seungmin <boscohyun@gmail.com>
2024-06-13 11:56:22 +0900

    introduce ActionPointRepository
    # Conflicts:
    #	Mimir/Program.cs

--- Mimir/Program.cs
+++ Mimir/Program.cs
@@ -31,6 +31,7 @@ builder.Services.AddSingleton<TableSheetsRepository>();
 builder.Services.AddSingleton<MetadataRepository>();
 builder.Services.AddSingleton<AgentRepository>();
 builder.Services.AddSingleton<AvatarRepository>();
+builder.Services.AddSingleton<ActionPointRepository>();
 builder.Services.AddSingleton<DailyRewardRepository>();
 builder.Services.AddSingleton<InventoryRepository>();
 builder.Services.AddSingleton<AllRuneRepository>();
