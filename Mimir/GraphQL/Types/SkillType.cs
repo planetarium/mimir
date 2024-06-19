61802d2203ba55991776fc2ce6f2697f792f803f
Hyun Seungmin <boscohyun@gmail.com>
2024-06-19 09:53:40 +0900

    introduce SkillObject

---
+++ Mimir/GraphQL/Types/SkillType.cs
@@ -0,0 +1,15 @@
+using Lib9c.GraphQL.Types;
+using Mimir.GraphQL.Objects;
+
+namespace Mimir.GraphQL.Types;
+
+public class SkillType : ObjectType<SkillObject>
+{
+    protected override void Configure(IObjectTypeDescriptor<SkillObject> descriptor)
+    {
+        descriptor
+            .Field(f => f.SkillRow)
+            .Description("The SkillSheet.Row of the skill.")
+            .Type<NonNullType<SkillSheetRowType>>();
+    }
+}
