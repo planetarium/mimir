db1d5f294da67a530d9d2493f54be0a536be6b58
Hyun Seungmin <boscohyun@gmail.com>
2024-06-20 21:14:06 +0900

    introduce RaidHandler for update item slot collection

---
+++ Mimir.Worker/Handler/RaidHandler.cs
@@ -0,0 +1,32 @@
+using Lib9c.Abstractions;
+using Libplanet.Action;
+using Mimir.Worker.CollectionUpdaters;
+using Mimir.Worker.Services;
+using Nekoyume.Model.EnumType;
+using Serilog;
+
+namespace Mimir.Worker.Handler;
+
+public class RaidHandler(IStateService stateService, MongoDbService store) :
+    BaseActionHandler(
+        stateService,
+        store,
+        "^raid[0-9]*$",
+        Log.ForContext<RaidHandler>())
+{
+    protected override async Task HandleAction(long blockIndex, IAction action)
+    {
+        if (action is not IRaidV2 raid)
+        {
+            throw new NotImplementedException(
+                $"Action is not {nameof(IRaidV2)}: {action.GetType()}");
+        }
+
+        await ItemSlotCollectionUpdater.UpdateAsync(
+            Store,
+            BattleType.Raid,
+            raid.AvatarAddress,
+            raid.CostumeIds,
+            raid.EquipmentIds);
+    }
+}
