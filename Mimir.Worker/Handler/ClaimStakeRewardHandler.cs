1a5f026815772c2f5f4cd0cb3970c6ba2e664cbd
Hyun Seungmin <boscohyun@gmail.com>
2024-06-21 18:28:46 +0900

    introduce ClaimStakeRewardHandler

---
+++ Mimir.Worker/Handler/ClaimStakeRewardHandler.cs
@@ -0,0 +1,33 @@
+using Lib9c.Abstractions;
+using Libplanet.Action;
+using Libplanet.Crypto;
+using Mimir.Worker.CollectionUpdaters;
+using Mimir.Worker.Services;
+using Serilog;
+
+namespace Mimir.Worker.Handler;
+
+public class ClaimStakeRewardHandler(IStateService stateService, MongoDbService store) :
+    BaseActionHandler(
+        stateService,
+        store,
+        "^claim_stake_reward[0-9]*$",
+        Log.ForContext<StakeHandler>())
+{
+    protected override async Task HandleAction(
+        long blockIndex,
+        Address signer,
+        IAction action)
+    {
+        if (action is not IClaimStakeRewardV1)
+        {
+            throw new NotImplementedException(
+                $"Action is not {nameof(IClaimStakeRewardV1)}: {action.GetType()}");
+        }
+
+        await StakeCollectionUpdater.UpdateAsync(
+            StateService,
+            Store,
+            signer);
+    }
+}
