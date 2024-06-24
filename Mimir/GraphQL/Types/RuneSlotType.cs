07bb187b6bc73d482f6d5e7bbc24d1559386425a
Hyun Seungmin <boscohyun@gmail.com>
2024-06-24 17:55:12 +0900

    introduce RuneSlotType

---
+++ Mimir/GraphQL/Types/RuneSlotType.cs
@@ -0,0 +1,31 @@
+using Nekoyume.Model.Rune;
+
+namespace Mimir.GraphQL.Types;
+
+public class RuneSlotType : ObjectType<RuneSlot>
+{
+    protected override void Configure(IObjectTypeDescriptor<RuneSlot> descriptor)
+    {
+        descriptor
+            .Field(f => f.Index)
+            .Description("The index of the rune slot.")
+            .Type<NonNullType<IntType>>();
+        descriptor
+            .Field(f => f.RuneSlotType)
+            .Description("The type of the rune slot.")
+            .Type<NonNullType<EnumType<Nekoyume.Model.EnumType.RuneSlotType>>>();
+        descriptor
+            .Field(f => f.RuneType)
+            .Description("The type of the rune.")
+            .Type<NonNullType<EnumType<Nekoyume.Model.EnumType.RuneType>>>();
+        descriptor
+            .Field(f => f.IsLock)
+            .Description("Whether the rune slot is locked.")
+            .Type<NonNullType<BooleanType>>();
+        descriptor
+            .Field(f => f.RuneId)
+            .Name("runeSheetId")
+            .Description("The RuneSheet ID of the rune equipped in the rune slot.")
+            .Type<IntType>();
+    }
+}
