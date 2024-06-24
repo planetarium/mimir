261b756158e328d9748da22020475ebed71b06de
Hyun Seungmin <boscohyun@gmail.com>
2024-06-24 12:12:16 +0900

    introduce ItemSlotType

---
+++ Mimir/GraphQL/Types/ItemSlotType.cs
@@ -0,0 +1,24 @@
+using Lib9c.GraphQL.Types;
+using Nekoyume.Model.EnumType;
+using Nekoyume.Model.State;
+
+namespace Mimir.GraphQL.Types;
+
+public class ItemSlotType : ObjectType<ItemSlotState>
+{
+    protected override void Configure(IObjectTypeDescriptor<ItemSlotState> descriptor)
+    {
+        descriptor
+            .Field(f => f.BattleType)
+            .Description("The type of battle that the item slot is used for.")
+            .Type<NonNullType<EnumType<BattleType>>>();
+        descriptor
+            .Field(f => f.Costumes)
+            .Description("The non-fungible item IDs of the costumes equipped in the item slot.")
+            .Type<NonNullType<ListType<GuidType>>>();
+        descriptor
+            .Field(f => f.Equipments)
+            .Description("The non-fungible item IDs of the equipments equipped in the item slot.")
+            .Type<NonNullType<ListType<GuidType>>>();
+    }
+}
