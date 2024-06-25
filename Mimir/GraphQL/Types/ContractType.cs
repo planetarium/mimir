dcca60eaedb28b2a9a7ab3c845d6415b3b3b0e8a
Hyun Seungmin <boscohyun@gmail.com>
2024-06-25 10:46:26 +0900

    introduce ContractType

---
+++ Mimir/GraphQL/Types/ContractType.cs
@@ -0,0 +1,27 @@
+using Nekoyume.Model.Stake;
+
+namespace Mimir.GraphQL.Types;
+
+public class ContractType : ObjectType<Contract>
+{
+    protected override void Configure(IObjectTypeDescriptor<Contract> descriptor)
+    {
+        descriptor.BindFieldsExplicitly();
+        descriptor
+            .Field(f => f.StakeRegularFixedRewardSheetTableName)
+            .Description("The name of the table that contains the regular fixed rewards for staking.")
+            .Type<NonNullType<StringType>>();
+        descriptor
+            .Field(f => f.StakeRegularRewardSheetTableName)
+            .Description("The name of the table that contains the regular rewards for staking.")
+            .Type<NonNullType<StringType>>();
+        descriptor
+            .Field(f => f.RewardInterval)
+            .Description("The interval at which rewards are given.")
+            .Type<NonNullType<LongType>>();
+        descriptor
+            .Field(f => f.LockupInterval)
+            .Description("The interval at which the stake is locked up.")
+            .Type<NonNullType<LongType>>();
+    }
+}
