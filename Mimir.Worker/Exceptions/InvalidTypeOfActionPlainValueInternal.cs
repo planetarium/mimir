356458b19a6905ab43771e94d3496c9537145bdc
Hyun Seungmin <boscohyun@gmail.com>
2024-06-25 18:12:39 +0900

    introduce InvalidTypeOfActionPlainValueInternalException

---
+++ Mimir.Worker/Exceptions/InvalidTypeOfActionPlainValueInternal.cs
@@ -0,0 +1,22 @@
+namespace Mimir.Worker.Exceptions;
+
+public class InvalidTypeOfActionPlainValueInternalException : Exception
+{
+    public InvalidTypeOfActionPlainValueInternalException(
+        string? message,
+        Exception? innerException = null) :
+        base(message, innerException)
+    {
+    }
+
+    public InvalidTypeOfActionPlainValueInternalException(
+        Bencodex.Types.ValueKind[] expected,
+        Bencodex.Types.ValueKind? actual,
+        Exception? innerException = null) :
+        base(
+            $"Invalid type of action plain value internal." +
+            $" Expected: {string.Join(", ", expected)}, Actual: {actual?.ToString() ?? "null"}",
+            innerException)
+    {
+    }
+}
