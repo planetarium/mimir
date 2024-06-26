88851a5f20f55a45500b57126d9306859275afa1
Hyun Seungmin <boscohyun@gmail.com>
2024-06-26 17:01:29 +0900

    fix StakeStateType

--- Mimir/GraphQL/Types/StakeStateType.cs
+++ Mimir/GraphQL/Types/StakeStateType.cs
@@ -8,17 +8,20 @@ public class StakeStateType : ObjectType<StakeStateV2>
     {
         descriptor.BindFieldsExplicitly();
         descriptor
-            .Field(f => f.Contract)
+            .Field("Contract")
             .Description("The contract of the stake.")
-            .Type<NonNullType<ContractType>>();
+            .Type<NonNullType<ContractType>>()
+            .Resolve(context => context.Parent<StakeStateV2>().Contract);
         descriptor
-            .Field(f => f.StartedBlockIndex)
+            .Field("StartedBlockIndex")
             .Description("The block index when the stake started.")
-            .Type<NonNullType<LongType>>();
+            .Type<NonNullType<LongType>>()
+            .Resolve(context => context.Parent<StakeStateV2>().StartedBlockIndex);
         descriptor
-            .Field(f => f.ReceivedBlockIndex)
+            .Field("ReceivedBlockIndex")
             .Description("The block index when the stake received. If 0, the stake is not received yet.")
-            .Type<NonNullType<LongType>>();
+            .Type<NonNullType<LongType>>()
+            .Resolve(context => context.Parent<StakeStateV2>().ReceivedBlockIndex);
         descriptor
             .Field(f => f.CancellableBlockIndex)
             .Description("The block index when the stake can be cancelled.")
