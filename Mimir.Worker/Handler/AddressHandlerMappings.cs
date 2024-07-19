e721e7e83973d6cd9c006b6c2b16b37cf2acb127
Hyun Seungmin <boscohyun@gmail.com>
2024-07-19 13:24:57 +0900

    register BountyBoardHandler to AddressHandlerMappings

--- Mimir.Worker/Handler/AddressHandlerMappings.cs
+++ Mimir.Worker/Handler/AddressHandlerMappings.cs
@@ -11,7 +11,8 @@ public static class AddressHandlerMappings
 {
     public static readonly Dictionary<Address, IStateHandler<StateData>> HandlerMappings = new();
 
-    public static readonly Currency OdinNCGCurrency = Currency.Legacy("NCG", 2, new Address("0x47d082a115c63e7b58b1532d20e631538eafadde"));
+    public static readonly Currency OdinNCGCurrency =
+        Currency.Legacy("NCG", 2, new Address("0x47d082a115c63e7b58b1532d20e631538eafadde"));
 
     static AddressHandlerMappings()
     {
@@ -25,6 +26,7 @@ public static class AddressHandlerMappings
         HandlerMappings.Add(Addresses.Collection, new CollectionStateHandler());
         HandlerMappings.Add(Addresses.DailyReward, new DailyRewardStateHandler());
         HandlerMappings.Add(Addresses.AdventureBoss, new SeasonInfoHandler());
+        HandlerMappings.Add(Addresses.BountyBoard, new BountyBoardHandler());
 
         RegisterBalanceHandler(OdinNCGCurrency);
         RegisterBalanceHandler(Currencies.Crystal);
