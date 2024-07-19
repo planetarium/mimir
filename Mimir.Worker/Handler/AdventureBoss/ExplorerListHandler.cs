bfd28f835cc4c30fab2d06a1f1f41c886f8ee0b4
Hyun Seungmin <boscohyun@gmail.com>
2024-07-19 16:14:41 +0900

    introduce ExplorerListHandler

---
+++ Mimir.Worker/Handler/AdventureBoss/ExplorerListHandler.cs
@@ -0,0 +1,29 @@
+using Bencodex.Types;
+using Mimir.Worker.Models;
+using Mimir.Worker.Models.State.AdventureBoss;
+using Mimir.Worker.Services;
+using Nekoyume.Model.AdventureBoss;
+
+namespace Mimir.Worker.Handler.AdventureBoss;
+
+public class ExplorerListHandler : IStateHandler<StateData>
+{
+    public StateData ConvertToStateData(StateDiffContext context) =>
+        new(context.Address, ConvertToState(context.RawState));
+
+    public async Task StoreStateData(MongoDbService store, StateData stateData) =>
+        await store.UpsertStateDataAsyncWithLinkAvatar(stateData);
+
+    private static ExplorerListState ConvertToState(IValue state)
+    {
+        if (state is not List list)
+        {
+            throw new InvalidCastException(
+                $"{nameof(state)} Invalid state type. Expected {nameof(List)}, got {state.GetType().Name}."
+            );
+        }
+
+        var explorerList = new ExplorerList(list);
+        return new ExplorerListState(explorerList);
+    }
+}
