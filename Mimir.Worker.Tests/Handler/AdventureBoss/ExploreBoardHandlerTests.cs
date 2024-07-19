485ce1b1f9d211ee6aad1f07b91a0caf02c6980c
Hyun Seungmin <boscohyun@gmail.com>
2024-07-19 17:10:50 +0900

    apply ExplorerState to ExploreBoardHandler

--- Mimir.Worker.Tests/Handler/AdventureBoss/ExploreBoardHandlerTests.cs
+++ Mimir.Worker.Tests/Handler/AdventureBoss/ExploreBoardHandlerTests.cs
@@ -1,6 +1,8 @@
 using System.Numerics;
+using Bencodex.Types;
 using Lib9c;
 using Libplanet.Crypto;
+using Mimir.Worker.Exceptions;
 using Mimir.Worker.Handler;
 using Mimir.Worker.Handler.AdventureBoss;
 using Mimir.Worker.Models.State.AdventureBoss;
@@ -15,7 +17,7 @@ public class ExploreBoardHandlerTests
     [Theory]
     [InlineData(0, false)]
     [InlineData(int.MaxValue, true)]
-    public void ConvertToStateData(long season, bool initOthers)
+    public void ConvertToBencodable_ExploreBoard(long season, bool initOthers)
     {
         var address = new PrivateKey().Address;
         var state = new ExploreBoard(season);
@@ -54,5 +56,57 @@ public class ExploreBoardHandlerTests
         Assert.Equal(state.RaffleWinner, obj.RaffleWinner);
         Assert.Equal(state.RaffleWinnerName, obj.RaffleWinnerName);
         Assert.Equal(state.RaffleReward, obj.RaffleReward);
+    }
+
+    [Theory]
+    [InlineData(0, false)]
+    [InlineData(int.MaxValue, true)]
+    public void ConvertToBencodable_Explorer(long season, bool initOthers)
+    {
+        var address = new PrivateKey().Address;
+        var state = new Explorer(
+            avatarAddress: new PrivateKey().Address,
+            name: "name");
+        if (initOthers)
+        {
+            state.Score = int.MaxValue;
+            state.Floor = int.MaxValue;
+            state.MaxFloor = int.MaxValue;
+            state.UsedApPotion = int.MaxValue;
+            state.UsedGoldenDust = int.MaxValue;
+            state.UsedNcg = int.MaxValue;
+            state.Claimed = true;
+        }
+
+        var context = new StateDiffContext
+        {
+            Address = address,
+            RawState = state.Bencoded,
+        };
+        var stateData = _handler.ConvertToStateData(context);
+
+        Assert.IsType<ExplorerState>(stateData.State);
+        var dataState = (ExplorerState)stateData.State;
+        var obj = dataState.Object;
+        Assert.Equal(state.AvatarAddress, obj.AvatarAddress);
+        Assert.Equal(state.Name, obj.Name);
+        Assert.Equal(state.Score, obj.Score);
+        Assert.Equal(state.Floor, obj.Floor);
+        Assert.Equal(state.MaxFloor, obj.MaxFloor);
+        Assert.Equal(state.UsedApPotion, obj.UsedApPotion);
+        Assert.Equal(state.UsedGoldenDust, obj.UsedGoldenDust);
+        Assert.Equal(state.UsedNcg, obj.UsedNcg);
+        Assert.Equal(state.Claimed, obj.Claimed);
+    }
+
+    [Fact]
+    public void ConvertToBencodable_Throw_UnexpectedStateException()
+    {
+        var context = new StateDiffContext
+        {
+            Address = new PrivateKey().Address,
+            RawState = List.Empty,
+        };
+        Assert.Throws<UnexpectedStateException>(() => _handler.ConvertToStateData(context));
     }
 }
