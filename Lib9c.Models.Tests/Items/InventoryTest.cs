251872cbf01d33bfc672ffe093d6222b8f003d03
Hyun Seungmin <boscohyun@gmail.com>
2024-07-25 19:59:10 +0900

    introduce Inventory

---
+++ Lib9c.Models.Tests/Items/InventoryTest.cs
@@ -0,0 +1,49 @@
+using Bencodex.Types;
+using Lib9c.Models.Item;
+using Lib9c.Models.Tests.Fixtures.States;
+
+namespace Lib9c.Models.Tests.Items;
+
+public class InventoryTest
+{
+    [Fact]
+    public void Test()
+    {
+        // Prepare target state
+        var value = StateReader.ReadState("Inventory");
+        var target = new Nekoyume.Model.Item.Inventory((List)value);
+
+        // serialize target state and deserialize as paired state
+        var serialized = target.Serialize();
+        var paired = new Inventory(serialized);
+        Assert.Equal(target.Items.Count, paired.Items.Count);
+        // ...
+
+        // serialize paired state and verify
+        var bencoded = paired.Bencoded;
+        Assert.Equal(serialized, bencoded);
+        // var serializedList = (List)serialized;
+        // var bencodedList = (List)bencoded;
+        // for (var i = 0; i < serializedList.Count; i++)
+        // {
+        //     var serializedItem = serializedList[i];
+        //     var bencodedItem = bencodedList[i];
+        //     Assert.Equal(serializedItem, bencodedItem);
+        // }
+
+        // deserialize bencoded state as target2 and verify
+        var target2 = new Nekoyume.Model.Item.Inventory((List)bencoded);
+        var serialized2 = target2.Serialize();
+        var target3 = new Nekoyume.Model.Item.Inventory((List)serialized);
+        var serialized3 = target3.Serialize();
+        Assert.Equal(serialized2, serialized3);
+        // var serialized2List = (List)serialized2;
+        // var serialized3List = (List)serialized3;
+        // for (var i = 0; i < serialized2List.Count; i++)
+        // {
+        //     var serialized2Item = serialized2List[i];
+        //     var serialized3Item = serialized3List[i];
+        //     Assert.Equal(serialized2Item, serialized3Item);
+        // }
+    }
+}
---
+++ Lib9c.Models/Item/Inventory.cs
@@ -0,0 +1,36 @@
+using Bencodex;
+using Bencodex.Types;
+using Lib9c.Models.Exceptions;
+using ValueKind = Bencodex.Types.ValueKind;
+
+namespace Lib9c.Models.Item;
+
+/// <summary>
+/// <see cref="Nekoyume.Model.Item.Inventory"/>
+/// </summary>
+public record Inventory : IBencodable
+{
+    public List<InventoryItem> Items { get; init; }
+
+    public IValue Bencoded => new List(Items
+        .OrderBy(i => i.Item.Id)
+        .ThenByDescending(i => i.Count)
+        .Select(i => i.Bencoded));
+
+    public Inventory(IValue bencoded)
+    {
+        if (bencoded is not List l)
+        {
+            throw new UnsupportedArgumentTypeException<ValueKind>(
+                nameof(bencoded),
+                [ValueKind.List],
+                bencoded.Kind);
+        }
+
+        Items = new List<InventoryItem>(l.Count);
+        foreach (var value in l)
+        {
+            Items.Add(new InventoryItem(value));
+        }
+    }
+}
