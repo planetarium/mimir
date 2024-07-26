2ecebd1a0778378f9e56315de80e4bcfae3d080b
Hyun Seungmin <boscohyun@gmail.com>
2024-07-26 15:32:27 +0900

    replace BigIntegerToStringConverter to Mimir.MongoDB project

--- Mimir.MongoDB/Json/Converters/BigIntegerToStringConverter.cs
+++ Mimir.MongoDB/Json/Converters/BigIntegerToStringConverter.cs
@@ -1,7 +1,7 @@
 using System.Numerics;
 using Newtonsoft.Json;
 
-namespace Mimir.Worker.Util;
+namespace Mimir.Worker.Json.Converters;
 
 public class BigIntegerToStringConverter : JsonConverter
 {
