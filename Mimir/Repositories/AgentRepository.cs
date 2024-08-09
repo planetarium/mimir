09d68611a2cee76f4799e319077fe97989ac05e0
Hyun Seungmin <boscohyun@gmail.com>
2024-08-09 19:44:59 +0900

    remove useless method

--- Mimir/Repositories/AgentRepository.cs
+++ Mimir/Repositories/AgentRepository.cs
@@ -1,11 +1,8 @@
 using Libplanet.Crypto;
 using Mimir.Enums;
 using Mimir.Exceptions;
-using Mimir.Models;
 using Mimir.MongoDB.Bson;
-using Mimir.MongoDB.Exceptions;
 using Mimir.Services;
-using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Mimir.Repositories;
@@ -25,30 +22,5 @@ public class AgentRepository(MongoDbService dbService)
         }
 
         return document;
-    }
-
-    public Agent GetAgent(Address agentAddress)
-    {
-        var collection = dbService.GetCollection<BsonDocument>(CollectionNames.Agent.Value);
-        var filter = Builders<BsonDocument>.Filter.Eq("Address", agentAddress.ToHex());
-        var document = collection.Find(filter).FirstOrDefault();
-        if (document is null)
-        {
-            throw new DocumentNotFoundInMongoCollectionException(
-                collection.CollectionNamespace.CollectionName,
-                $"'Address' equals to '{agentAddress.ToHex()}'");
-        }
-
-        try
-        {
-            var doc = document["Object"].AsBsonDocument;
-            return new Agent(doc);
-        }
-        catch (KeyNotFoundException e)
-        {
-            throw new KeyNotFoundInBsonDocumentException(
-                "document[\"State\"][\"Object\"].AsBsonDocument",
-                e);
-        }
     }
 }
