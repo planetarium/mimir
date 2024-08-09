4f077cc787357affd94197408b61e9512d9488bd
Hyun Seungmin <boscohyun@gmail.com>
2024-08-09 18:36:35 +0900

    change type of exception

--- Mimir.MongoDB/Bson/Serialization/Serializers/AddressBsonSerializer.cs
+++ Mimir.MongoDB/Bson/Serialization/Serializers/AddressBsonSerializer.cs
@@ -1,4 +1,5 @@
 using Libplanet.Crypto;
+using Mimir.MongoDB.Exceptions;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
@@ -10,8 +11,13 @@ public class AddressBsonSerializer : SerializerBase<Address>
     public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, Address value) =>
         context.Writer.WriteString(value.ToHex());
 
-    public override Address Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args) =>
-        context.Reader.GetCurrentBsonType() is BsonType.String
+    public override Address Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+    {
+        var bsonType = context.Reader.GetCurrentBsonType();
+        return bsonType is BsonType.String
             ? new Address(context.Reader.ReadString())
-            : throw new Exception();
+            : throw new UnexpectedTypeOfBsonValueException(
+                [BsonType.String],
+                bsonType);
+    }
 }
