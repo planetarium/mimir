1359431ea19ae6654a0a17fc918ca7c6657afe27
Hyun Seungmin <boscohyun@gmail.com>
2024-08-09 19:44:13 +0900

    add Query.GetAgentAsync

--- Mimir/GraphQL/Queries/Query.cs
+++ Mimir/GraphQL/Queries/Query.cs
@@ -1,5 +1,16 @@
+using Lib9c.Models.States;
+using Libplanet.Crypto;
+using Mimir.Repositories;
+
 namespace Mimir.GraphQL.Queries;
 
 public class Query
 {
+    public async Task<AgentState> GetAgentAsync(
+        Address address,
+        [Service] AgentRepository repo)
+    {
+        var doc = await repo.GetAgentAsync(address);
+        return doc.Object;
+    }
 }
