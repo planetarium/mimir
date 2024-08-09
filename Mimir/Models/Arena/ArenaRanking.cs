260915ba986820c7f95872378a6aa28e886b3772
Hyun Seungmin <boscohyun@gmail.com>
2024-08-09 17:59:36 +0900

    change ArenaRanking to record

--- Mimir/Models/Arena/ArenaRanking.cs
+++ Mimir/Models/Arena/ArenaRanking.cs
@@ -2,30 +2,20 @@ using System.Text.Json.Serialization;
 
 namespace Mimir.Models.Arena;
 
-public class ArenaRanking(
-    string avatarAddress,
-    string arenaAddress,
-    int win,
-    int lose,
-    long rank,
-    int ticket,
-    int ticketResetCount,
-    int purchasedTicketCount,
-    int score
-)
+public record ArenaRanking(
+    string AvatarAddress,
+    string ArenaAddress,
+    int Win,
+    int Lose,
+    long Rank,
+    int Ticket,
+    int TicketResetCount,
+    int PurchasedTicketCount,
+    int Score)
 {
-    public string AvatarAddress { get; set; } = avatarAddress;
-    public string ArenaAddress { get; set; } = arenaAddress;
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public int? CP { get; set; }
-    public int Win { get; set; } = win;
-    public int Lose { get; set; } = lose;
-    public long Rank { get; set; } = rank;
-    public int Ticket { get; set; } = ticket;
-    public int TicketResetCount { get; set; } = ticketResetCount;
-    public int PurchasedTicketCount { get; set; } = purchasedTicketCount;
-    public int Score { get; set; } = score;
-
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public Avatar? Avatar { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? CP { get; set; }
 }
