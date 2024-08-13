833949fe6dc59764ec5920b4a8bae44b80c47b5f
Hyun Seungmin <boscohyun@gmail.com>
2024-08-13 11:22:36 +0900

    introduce Lib9c.Models.Arena.ArenaScore

---
+++ Lib9c.Models/Arena/ArenaScore.cs
@@ -0,0 +1,32 @@
+using Bencodex;
+using Bencodex.Types;
+using Lib9c.Models.Exceptions;
+using Libplanet.Crypto;
+using Nekoyume.Model.State;
+using ValueKind = Bencodex.Types.ValueKind;
+
+namespace Lib9c.Models.Arena;
+
+public record ArenaScore : IBencodable
+{
+    public Address Address { get; init; }
+    public int Score { get; init; }
+
+    public IValue Bencoded => List.Empty
+        .Add(Address.Serialize())
+        .Add(Score);
+
+    public ArenaScore(IValue bencoded)
+    {
+        if (bencoded is not List l)
+        {
+            throw new UnsupportedArgumentTypeException<ValueKind>(
+                nameof(bencoded),
+                new[] { ValueKind.List },
+                bencoded.Kind);
+        }
+
+        Address = l[0].ToAddress();
+        Score = (Integer)l[1];
+    }
+}
