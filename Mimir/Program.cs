92d4af85b3a04a651f8499081e459908619bada5
Atralupus <me@atralupus.com>
2024-08-21 18:58:59 +0900

    Remove controllers

--- Mimir/Program.cs
+++ Mimir/Program.cs
@@ -17,9 +17,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddEnvironmentVariables();
 
-builder.Services.Configure<HeadlessStateServiceOption>(
-    builder.Configuration.GetRequiredSection(HeadlessStateServiceOption.SectionName)
-);
 builder.Services.Configure<DatabaseOption>(
     builder.Configuration.GetRequiredSection(DatabaseOption.SectionName)
 );
@@ -35,39 +32,7 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 );
 
 builder.Services.AddAuthorization();
-builder
-    .Services.AddAuthentication("Bearer")
-    .AddJwtBearer(
-        JwtBearerDefaults.AuthenticationScheme,
-        options =>
-        {
-            var jwtOptions = builder
-                .Configuration.GetRequiredSection(JwtOption.SectionName)
-                .Get<JwtOption>();
-
-            if (jwtOptions is null)
-            {
-                throw new InvalidOperationException("JWT options are not configured properly.");
-            }
-
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = false,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = jwtOptions.Issuer,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Key))
-            };
-        }
-    );
-
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SupportNonNullableReferenceTypes();
-});
-builder.Services.AddTransient<IStateService, HeadlessStateService>();
 builder.Services.AddSingleton<MongoDbService>();
 builder.Services.AddSingleton<TableSheetsRepository>();
 builder.Services.AddSingleton<MetadataRepository>();
@@ -86,36 +51,6 @@ builder.Services.AddSingleton<ArenaRepository>();
 // builder.Services.AddSingleton<StakeRepository>();
 // builder.Services.AddSingleton<ProductRepository>();
 // builder.Services.AddSingleton<SeasonInfoRepository>();
-builder.Services.AddControllers();
-builder
-    .Services.AddHeadlessGQLClient()
-    .ConfigureHttpClient(
-        (provider, client) =>
-        {
-            var headlessStateServiceOption = provider.GetRequiredService<
-                IOptions<HeadlessStateServiceOption>
-            >();
-            var option = headlessStateServiceOption.Value;
-            client.BaseAddress = new Uri(option.Endpoint);
-
-            if (option.JwtSecretKey is not null && option.JwtIssuer is not null)
-            {
-                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(option.JwtSecretKey));
-                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-                var token = new JwtSecurityToken(
-                    issuer: option.JwtIssuer,
-                    expires: DateTime.UtcNow.AddMinutes(5),
-                    signingCredentials: creds
-                );
-
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
-                    "Bearer",
-                    new JwtSecurityTokenHandler().WriteToken(token)
-                );
-            }
-        }
-    );
 builder.Services.AddCors();
 builder.Services.AddHttpClient();
 builder.Services
@@ -172,8 +107,6 @@ app.UseRouting();
 app.MapGet("/", () => "Health Check").RequireRateLimiting("jwt");
 app.MapGraphQL().RequireRateLimiting("jwt");
 
-app.UseSwagger();
-app.UseSwaggerUI();
 app.UseAuthorization();
 app.UseRateLimiter();
 app.UseHttpsRedirection();
