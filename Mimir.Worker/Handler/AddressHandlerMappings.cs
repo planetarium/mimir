8539ddf800e8abcf4126c5fad21abe6124162345
Atralupus <me@atralupus.com>
2024-09-07 23:06:08 +0900

    Uncomment NCG Handler temporarily

--- Mimir.Worker/Handler/AddressHandlerMappings.cs
+++ Mimir.Worker/Handler/AddressHandlerMappings.cs
@@ -1,6 +1,7 @@
 using Lib9c;
 using Libplanet.Crypto;
 using Libplanet.Types.Assets;
+using Mimir.Worker.Constants;
 using Nekoyume;
 
 namespace Mimir.Worker.Handler;
@@ -9,8 +10,16 @@ public static class AddressHandlerMappings
 {
     public static readonly Dictionary<Address, IStateHandler> HandlerMappings = new();
 
-    public static readonly Currency OdinNCGCurrency =
-        Currency.Legacy("NCG", 2, new Address("0x47d082a115c63e7b58b1532d20e631538eafadde"));
+    public static readonly Currency OdinNCGCurrency = Currency.Legacy(
+        "NCG",
+        2,
+        new Address("0x47d082a115c63e7b58b1532d20e631538eafadde")
+    );
+    public static readonly Currency HeimdallNCGCurrency = Currency.Legacy(
+        "NCG",
+        2,
+        new Address("0x47d082a115c63e7b58b1532d20e631538eafadde")
+    );
 
     static AddressHandlerMappings()
     {
@@ -28,7 +37,6 @@ public static class AddressHandlerMappings
         // // HandlerMappings.Add(Addresses.ExploreBoard, new ExploreBoardHandler());
         // // HandlerMappings.Add(Addresses.ExplorerList, new ExplorerListHandler());
 
-        RegisterBalanceHandler(OdinNCGCurrency);
         RegisterBalanceHandler(Currencies.Crystal);
         RegisterBalanceHandler(Currencies.StakeRune);
         RegisterBalanceHandler(Currencies.DailyRewardRune);
@@ -36,14 +44,28 @@ public static class AddressHandlerMappings
         RegisterBalanceHandler(Currencies.Mead);
         RegisterBalanceHandler(Currencies.FreyaBlessingRune);
         RegisterBalanceHandler(Currencies.FreyaLiberationRune);
-        RegisterBalanceHandler(Currencies.OdinWeaknessRune);
-        RegisterBalanceHandler(Currencies.OdinWisdomRune);
+    }
+
+    public static void RegisterCurrencyHandler(PlanetType planet)
+    {
+        switch (planet.Value)
+        {
+            case var _ when planet == PlanetType.ODIN:
+                RegisterBalanceHandler(OdinNCGCurrency);
+                RegisterBalanceHandler(Currencies.OdinWeaknessRune);
+                RegisterBalanceHandler(Currencies.OdinWisdomRune);
+                break;
+            case var _ when planet == PlanetType.HEIMDALL:
+                RegisterBalanceHandler(HeimdallNCGCurrency);
+                break;
+            default:
+                throw new ArgumentException("Invalid planet");
+        }
     }
 
     private static void RegisterBalanceHandler(Currency currency)
     {
-        HandlerMappings.Add(
-            new Address(currency.Hash.ToByteArray()),
+        HandlerMappings.Add(new Address(currency.Hash.ToByteArray()),
 #pragma warning disable CS0618 // Type or member is obsolete
             new BalanceHandler(currency)
 #pragma warning restore CS0618 // Type or member is obsolete
