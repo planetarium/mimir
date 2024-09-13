10a2954a0e05206d1ef5d94b178636e58d54dd81
Hyun Seungmin <boscohyun@gmail.com>
2024-09-13 19:06:02 +0900

    add "balance" GraphQL query

--- Mimir/GraphQL/Queries/Query.cs
+++ Mimir/GraphQL/Queries/Query.cs
@@ -1,3 +1,6 @@
+using HotChocolate.AspNetCore;
+using Lib9c.GraphQL.Extensions;
+using Lib9c.GraphQL.InputObjects;
 using Lib9c.Models.States;
 using Libplanet.Crypto;
 using Mimir.GraphQL.Objects;
@@ -14,8 +17,35 @@ public class Query
     /// <param name="collectionName">The name of the collection.</param>
     /// <param name="repo"></param>
     /// <returns>The metadata</returns>
-    public async Task<MetadataDocument> GetMetadataAsync(string collectionName, [Service] MetadataRepository repo)
-        => await repo.GetByCollectionAsync(collectionName);
+    public async Task<MetadataDocument> GetMetadataAsync(string collectionName, [Service] MetadataRepository repo) =>
+        await repo.GetByCollectionAsync(collectionName);
+
+    /// <summary>
+    /// Get the balance of a specific currency for a given address.
+    /// Choose one of the following parameters to specify the currency: currency, currencyTicker
+    /// </summary>
+    /// <param name="currency">The currency object.</param>
+    /// <param name="currencyTicker">The ticker of the currency.</param>
+    /// <param name="address">The address of the balance.</param>
+    /// <exception cref="GraphQLRequestException"></exception>
+    public async Task<string> GetBalanceAsync(
+        CurrencyInput? currency,
+        string? currencyTicker,
+        Address address,
+        [Service] BalanceRepository repo)
+    {
+        if (currency is not null)
+        {
+            return (await repo.GetByAddressAsync(currency.ToCurrency(), address)).Object;
+        }
+
+        if (currencyTicker is not null)
+        {
+            return (await repo.GetByAddressAsync(currencyTicker.ToCurrency(), address)).Object;
+        }
+
+        throw new GraphQLRequestException("Either currency or currencyTicker must be provided.");
+    }
 
     /// <summary>
     /// Get an agent state by address.
