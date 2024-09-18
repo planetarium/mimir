d88ba2291b2ced521f9daeac8de88ef6b5a1ea2b
Atralupus <me@atralupus.com>
2024-09-18 23:11:33 +0900

    fix typo

--- Mimir.MongoDB/Bson/StakeDocument.cs
+++ Mimir.MongoDB/Bson/StakeDocument.cs
@@ -5,4 +5,4 @@ using MongoDB.Bson.Serialization.Attributes;
 namespace Mimir.MongoDB.Bson;
 
 [BsonIgnoreExtraElements]
-public record StakeDocument(Address Address, Address agentAddress, StakeState Object) : MimirBsonDocument(Address);
+public record StakeDocument(Address Address, Address AgentAddress, StakeState Object) : MimirBsonDocument(Address);
