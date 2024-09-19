76d024838589d2847e8875c85a5ce1db1ff013c6
Hyun Seungmin <boscohyun@gmail.com>
2024-09-19 19:24:17 +0900

    introduce MaterialAndIntDictionaryJsonConverter

---
+++ Mimir.MongoDB/Json/Converters/MaterialAndIntDictionaryJsonConverter.cs
@@ -0,0 +1,60 @@
+using Lib9c.Models.Items;
+using Mimir.MongoDB.Json.Extensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Mimir.MongoDB.Json.Converters;
+
+public class MaterialAndIntDictionaryJsonConverter : JsonConverter<Dictionary<Material, int>>
+{
+    public override Dictionary<Material, int>? ReadJson(
+        JsonReader reader,
+        Type objectType,
+        Dictionary<Material, int>? existingValue,
+        bool hasExistingValue,
+        JsonSerializer serializer)
+    {
+        if (reader.TokenType == JsonToken.Null)
+        {
+            return null;
+        }
+
+        var dictionary = new Dictionary<Material, int>();
+        var jObject = JObject.Load(reader);
+        foreach (var property in jObject.Properties())
+        {
+            var material = JsonConvert.DeserializeObject<Material>(property.Name);
+            if (material is null)
+            {
+                throw new JsonException($"Failed to deserialize {nameof(Material)}. {property.Name}");
+            }
+
+            var value = property.Value.ToObject<int>();
+            dictionary.Add(material, value);
+        }
+
+        return dictionary;
+    }
+
+    public override void WriteJson(
+        JsonWriter writer,
+        Dictionary<Material, int>? value,
+        JsonSerializer serializer)
+    {
+        if (value == null)
+        {
+            writer.WriteNull();
+            return;
+        }
+
+        writer.WriteStartObject();
+        foreach (var kvp in value)
+        {
+            var key = JsonConvert.SerializeObject(kvp.Key, MimirBsonDocumentExtensions.JsonSerializerSettings);
+            writer.WritePropertyName(key);
+            writer.WriteValue(kvp.Value);
+        }
+
+        writer.WriteEndObject();
+    }
+}
