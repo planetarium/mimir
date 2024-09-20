7ea3e6b04dff2bdb3f1000def43cb234ee3af825
Hyun Seungmin <boscohyun@gmail.com>
2024-09-20 10:29:21 +0900

    handling the StakeState can be null

--- Mimir.Worker/CollectionUpdaters/StakeCollectionUpdater.cs
+++ Mimir.Worker/CollectionUpdaters/StakeCollectionUpdater.cs
@@ -1,3 +1,4 @@
+using Bencodex.Types;
 using Lib9c.Models.States;
 using Libplanet.Crypto;
 using Mimir.MongoDB;
@@ -18,16 +19,20 @@ public static class StakeCollectionUpdater
     )
     {
         var stakeAddress = Nekoyume.Model.Stake.StakeStateV2.DeriveAddress(agentAddress);
-        if (await stateService.GetState(stakeAddress) is not { } bencoded)
+        var stakeState = await stateService.GetState(stakeAddress, stoppingToken);
+        StakeDocument document;
+        if (stakeState is null ||
+            stakeState.Kind == ValueKind.Null)
         {
-            return;
+            document = new StakeDocument(stakeAddress, agentAddress, null);
+        }
+        else
+        {
+            document = new StakeDocument(stakeAddress, agentAddress, new StakeState(stakeState));
         }
 
         try
         {
-            var stakeState = new StakeState(bencoded);
-            var document = new StakeDocument(stakeAddress, agentAddress, stakeState);
-
             await store.UpsertStateDataManyAsync(
                 CollectionNames.GetCollectionName<StakeDocument>(),
                 [document],
@@ -35,7 +40,7 @@ public static class StakeCollectionUpdater
                 stoppingToken
             );
         }
-        catch (System.ArgumentException)
+        catch (ArgumentException)
         {
             // Skip stake state v1
             return;
