21dc3e9355743580f96a6053e99b4fa542bf0b75
Hyun Seungmin <boscohyun@gmail.com>
2024-09-26 17:22:51 +0900

    clean up ProductsState

--- Lib9c.Models/States/ProductsState.cs
+++ Lib9c.Models/States/ProductsState.cs
@@ -2,14 +2,17 @@ using Bencodex;
 using Bencodex.Types;
 using Lib9c.Models.Exceptions;
 using Lib9c.Models.Extensions;
+using MongoDB.Bson.Serialization.Attributes;
 using ValueKind = Bencodex.Types.ValueKind;
 
 namespace Lib9c.Models.States;
 
+[BsonIgnoreExtraElements]
 public record ProductsState : IBencodable
 {
     public List<Guid> ProductIds { get; init; } = new();
 
+    [BsonIgnore, GraphQLIgnore]
     public IValue Bencoded => new List(ProductIds.Select(e => e.Serialize()));
 
     public ProductsState()
