2b9001746b2446b54dd7402ab3ed7744d388f0c1
Hyun Seungmin <boscohyun@gmail.com>
2024-09-27 20:04:22 +0900

    re-implement ProductRepository

--- Mimir/Repositories/ProductRepository.cs
+++ Mimir/Repositories/ProductRepository.cs
@@ -1,80 +1,42 @@
 using Bencodex;
-using Bencodex.Types;
-using Lib9c.Models.Factories;
-using Lib9c.Models.Market;
 using Mimir.Exceptions;
 using Mimir.MongoDB;
 using Mimir.MongoDB.Bson;
 using Mimir.Services;
-using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.GridFS;
 
 namespace Mimir.Repositories;
 
-public class ProductRepository(MongoDbService dbService)
+public class ProductRepository
 {
     private static readonly Codec Codec = new();
 
-    public async Task<ProductDocument> GetByProductIdAsync(Guid productId)
+    private readonly IMongoCollection<ProductDocument> _collection;
+    private readonly GridFSBucket _gridFsBucket;
+
+    public ProductRepository(MongoDbService dbService)
     {
         var collectionName = CollectionNames.GetCollectionName<ProductDocument>();
-        var collection = dbService.GetCollection<ProductDocument>(collectionName);
+        _collection = dbService.GetCollection<ProductDocument>(collectionName);
+        _gridFsBucket = dbService.GetGridFs();
+    }
+
+    public IExecutable<ProductDocument> GetAll() => _collection
+        .Find(Builders<ProductDocument>.Filter.Empty)
+        .AsExecutable();
+
+    public async Task<ProductDocument> GetByProductIdAsync(Guid productId)
+    {
         var filter = Builders<ProductDocument>.Filter.Eq(doc => doc.Object.ProductId, productId);
-        var productDocument = await collection.Find(filter).FirstOrDefaultAsync();
+        var productDocument = await _collection.Find(filter).FirstOrDefaultAsync();
         if (productDocument is null)
         {
             throw new DocumentNotFoundInMongoCollectionException(
-                collection.CollectionNamespace.CollectionName,
+                _collection.CollectionNamespace.CollectionName,
                 $"'ProductId' equals to '{productId}'");
         }
 
         return productDocument;
-    }
-
-    public List<Product> GetProducts(long skip, int limit)
-    {
-        var collectionName = CollectionNames.GetCollectionName<ProductDocument>();
-        return GetProducts(
-            dbService.GetCollection<ProductDocument>(collectionName),
-            skip,
-            limit
-        );
-    }
-
-    private List<Product> GetProducts(
-        IMongoCollection<ProductDocument> collection,
-        long skip,
-        int limit)
-    {
-        var pipelines = new List<BsonDocument> { new("$skip", skip), new("$limit", limit) };
-        var aggregation = collection.Aggregate<BsonDocument>(pipelines).ToList();
-        var products = aggregation
-            .Select(async doc =>
-                await GetRawState(dbService.GetGridFs(), doc["RawStateFileId"].AsObjectId))
-            .Select(doc => doc.Result)
-            .Where(doc => doc != null)
-            .Select(DeserializeProduct)
-            .ToList();
-
-        return products;
-    }
-
-    private async Task<byte[]> GetRawState(GridFSBucket gridFs, ObjectId rawStateFileId)
-    {
-        return await MongoDbService.RetrieveFromGridFs(gridFs, rawStateFileId);
-    }
-
-    public Product DeserializeProduct(byte[] rawState)
-    {
-        IValue ivalue = Codec.Decode(rawState);
-
-        if (ivalue is not List list)
-        {
-            throw new InvalidCastException(nameof(Product));
-        }
-
-        var product = ProductFactory.DeserializeProduct(list);
-        return product;
     }
 }
