b0467f08856addf39d2a59d905cd3614751b8938
Hyun Seungmin <boscohyun@gmail.com>
2024-09-27 20:05:17 +0900

    add "products" GraphQL query

--- Mimir/GraphQL/Types/QueryType.cs
+++ Mimir/GraphQL/Types/QueryType.cs
@@ -1,8 +1,17 @@
 using Mimir.GraphQL.Queries;
+using Mimir.GraphQL.Types.MimirBsonDocuments;
+using Mimir.Repositories;
 
 namespace Mimir.GraphQL.Types;
 
 public class QueryType : ObjectType<Query>
 {
-    protected override void Configure(IObjectTypeDescriptor<Query> descriptor) { }
+    protected override void Configure(IObjectTypeDescriptor<Query> descriptor)
+    {
+        descriptor
+            .Field("products")
+            .Description("Retrieves a list of market products.")
+            .UseOffsetPaging<ProductDocumentType>()
+            .Resolve(context => context.Service<ProductRepository>().GetAll());
+    }
 }
