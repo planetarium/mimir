074d0833bf057bcd07bc6d277e13b33e36f132e1
Hyun Seungmin <boscohyun@gmail.com>
2024-09-27 10:53:38 +0900

    update Bson serializations

--- Mimir.MongoDB/Bson/Serialization/Serializers/Libplanet/CurrencySerializer.cs
+++ Mimir.MongoDB/Bson/Serialization/Serializers/Libplanet/CurrencySerializer.cs
@@ -2,7 +2,6 @@ using System.Collections.Immutable;
 using System.Numerics;
 using Libplanet.Crypto;
 using Libplanet.Types.Assets;
-using Mimir.MongoDB.Bson.Serialization.Serializers.System;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
@@ -21,45 +20,7 @@ public class CurrencySerializer : StructSerializerBase<Currency>
         public const string MaximumSupplyMinor = "MaximumSupplyMinor";
     }
 
-    private static class Flags
-    {
-        public const long Ticker = 1L << 0;
-        public const long DecimalPlaces = 1L << 1;
-        public const long Minters = 1L << 2;
-        public const long TotalSupplyTrackable = 1L << 3;
-        public const long MaximumSupplyMajor = 1L << 4;
-        public const long MaximumSupplyMinor = 1L << 5;
-    }
-
     public static readonly CurrencySerializer Instance = new();
-
-    private readonly SerializerHelper _helper = new(
-        new SerializerHelper.Member(ElementNames.Ticker, Flags.Ticker),
-        new SerializerHelper.Member(ElementNames.DecimalPlaces, Flags.DecimalPlaces),
-        new SerializerHelper.Member(ElementNames.Minters, Flags.Minters),
-        new SerializerHelper.Member(ElementNames.TotalSupplyTrackable, Flags.TotalSupplyTrackable),
-        new SerializerHelper.Member(ElementNames.MaximumSupplyMajor, Flags.MaximumSupplyMajor),
-        new SerializerHelper.Member(ElementNames.MaximumSupplyMinor, Flags.MaximumSupplyMinor));
-
-    private readonly BsonSerializationInfo _decimalPlacesInfo = new(
-        ElementNames.DecimalPlaces,
-        new ByteSerializer(),
-        typeof(byte));
-
-    private readonly BsonSerializationInfo _mintersInfo = new(
-        ElementNames.Minters,
-        new ArraySerializer<AddressSerializer>(),
-        typeof(IImmutableSet<Address>));
-
-    private readonly BsonSerializationInfo _maximumSupplyMajorInfo = new(
-        ElementNames.MaximumSupplyMajor,
-        new NullableSerializer<BigInteger>(BigIntegerSerializer.Instance),
-        typeof(BigInteger?));
-
-    private readonly BsonSerializationInfo _maximumSupplyMinorInfo = new(
-        ElementNames.MaximumSupplyMinor,
-        new NullableSerializer<BigInteger>(BigIntegerSerializer.Instance),
-        typeof(BigInteger?));
 
     public static Currency Deserialize(BsonDocument doc)
     {
@@ -74,7 +35,7 @@ public class CurrencySerializer : StructSerializerBase<Currency>
         var maximumSupplyMajor = doc.TryGetValue(ElementNames.MaximumSupplyMajor, out var maximumSupplyMajorBsonValue)
             ? BigInteger.Parse(maximumSupplyMajorBsonValue.AsString)
             : (BigInteger?)null;
-        var maximumSupplyMinor = doc.TryGetValue(ElementNames.MaximumSupplyMajor, out var maximumSupplyMinorBsonValue)
+        var maximumSupplyMinor = doc.TryGetValue(ElementNames.MaximumSupplyMinor, out var maximumSupplyMinorBsonValue)
             ? BigInteger.Parse(maximumSupplyMinorBsonValue.AsString)
             : (BigInteger?)null;
         if (totalSupplyTrackable)
@@ -100,83 +61,38 @@ public class CurrencySerializer : StructSerializerBase<Currency>
         BsonDeserializationContext context,
         BsonDeserializationArgs args)
     {
-        var ticker = string.Empty;
-        byte decimalPlaces = default;
-        Address[]? minters = null;
-        bool totalSupplyTrackable = default;
-        BigInteger? maximumSupplyMajor = null;
-        BigInteger? maximumSupplyMinor = null;
-        _helper.DeserializeMembers(context, (_, flag) =>
-        {
-            switch (flag)
-            {
-                case Flags.Ticker:
-                    ticker = context.Reader.ReadString();
-                    break;
-                case Flags.DecimalPlaces:
-                    decimalPlaces = (byte)_decimalPlacesInfo.Serializer.Deserialize(context);
-                    break;
-                case Flags.Minters:
-                    minters = (Address[]?)_mintersInfo.Serializer.Deserialize(context);
-                    break;
-                case Flags.TotalSupplyTrackable:
-                    totalSupplyTrackable = context.Reader.ReadBoolean();
-                    break;
-                case Flags.MaximumSupplyMajor:
-                    maximumSupplyMajor = (BigInteger?)_maximumSupplyMajorInfo.Serializer.Deserialize(context);
-                    break;
-                case Flags.MaximumSupplyMinor:
-                    maximumSupplyMinor = (BigInteger?)_maximumSupplyMinorInfo.Serializer.Deserialize(context);
-                    break;
-            }
-        });
-
-        if (totalSupplyTrackable)
-        {
-            (BigInteger, BigInteger)? maximumSupply = maximumSupplyMajor.HasValue || maximumSupplyMinor.HasValue
-                ? (maximumSupplyMajor ?? BigInteger.Zero, maximumSupplyMinor ?? BigInteger.Zero)
-                : null;
-            return maximumSupply.HasValue
-                ? Currency.Capped(
-                    ticker,
-                    decimalPlaces,
-                    maximumSupply.Value,
-                    minters?.ToImmutableHashSet())
-                : Currency.Uncapped(ticker, decimalPlaces, minters?.ToImmutableHashSet());
-        }
-
-#pragma warning disable CS0618 // Type or member is obsolete
-        return Currency.Legacy(ticker, decimalPlaces, minters?.ToImmutableHashSet());
-#pragma warning restore CS0618 // Type or member is obsolete
+        var doc = BsonDocumentSerializer.Instance.Deserialize(context, args);
+        return Deserialize(doc);
     }
 
-    public override void Serialize(
-        BsonSerializationContext context,
-        BsonSerializationArgs args,
-        Currency value)
-    {
-        var writer = context.Writer;
-        writer.WriteStartDocument();
-        writer.WriteName(ElementNames.Ticker);
-        writer.WriteString(value.Ticker);
-        writer.WriteName(ElementNames.DecimalPlaces);
-        _decimalPlacesInfo.Serializer.Serialize(context, value.DecimalPlaces);
-        if (value.Minters is not null)
-        {
-            writer.WriteName(ElementNames.Minters);
-            _mintersInfo.Serializer.Serialize(context, value.Minters);
-        }
-
-        writer.WriteName(ElementNames.TotalSupplyTrackable);
-        writer.WriteBoolean(value.TotalSupplyTrackable);
-        if (value.MaximumSupply.HasValue)
-        {
-            writer.WriteName(ElementNames.MaximumSupplyMajor);
-            _maximumSupplyMajorInfo.Serializer.Serialize(context, value.MaximumSupply.Value.MajorUnit);
-            writer.WriteName(ElementNames.MaximumSupplyMinor);
-            _maximumSupplyMinorInfo.Serializer.Serialize(context, value.MaximumSupply.Value.MinorUnit);
-        }
-
-        writer.WriteEndDocument();
-    }
+    // DO NOT OVERRIDE Serialize METHOD: Currently objects will be serialized to Json first.
+    // public override void Serialize(
+    //     BsonSerializationContext context,
+    //     BsonSerializationArgs args,
+    //     Currency value)
+    // {
+    //     var writer = context.Writer;
+    //     writer.WriteStartDocument();
+    //     writer.WriteName(ElementNames.Ticker);
+    //     writer.WriteString(value.Ticker);
+    //     writer.WriteName(ElementNames.DecimalPlaces);
+    //     _decimalPlacesInfo.Serializer.Serialize(context, value.DecimalPlaces);
+    //     if (value.Minters is not null)
+    //     {
+    //         writer.WriteName(ElementNames.Minters);
+    //         _mintersInfo.Serializer.Serialize(context, value.Minters);
+    //     }
+    //
+    //     writer.WriteName(ElementNames.TotalSupplyTrackable);
+    //     writer.WriteBoolean(value.TotalSupplyTrackable);
+    //     if (value.MaximumSupply.HasValue)
+    //     {
+    //         writer.WriteName(ElementNames.MaximumSupplyMajor);
+    //         _maximumSupplyMajorInfo.Serializer.Serialize(context, value.MaximumSupply.Value.MajorUnit);
+    //         writer.WriteName(ElementNames.MaximumSupplyMinor);
+    //         _maximumSupplyMinorInfo.Serializer.Serialize(context, value.MaximumSupply.Value.MinorUnit);
+    //     }
+    //
+    //     writer.WriteEndDocument();
+    // }
 }
--- Mimir.MongoDB/Bson/Serialization/Serializers/Libplanet/FungibleAssetValueSerializer.cs
+++ Mimir.MongoDB/Bson/Serialization/Serializers/Libplanet/FungibleAssetValueSerializer.cs
@@ -1,6 +1,5 @@
 using System.Numerics;
 using Libplanet.Types.Assets;
-using Mimir.MongoDB.Bson.Serialization.Serializers.System;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
@@ -15,27 +14,7 @@ public class FungibleAssetValueSerializer : StructSerializerBase<FungibleAssetVa
         public const string RawValue = "RawValue";
     }
 
-    private static class Flags
-    {
-        public const long Currency = 1L << 0;
-        public const long RawValue = 1L << 1;
-    }
-
     public static readonly FungibleAssetValueSerializer Instance = new();
-
-    private readonly SerializerHelper _helper = new(
-        new SerializerHelper.Member(ElementNames.Currency, Flags.Currency),
-        new SerializerHelper.Member(ElementNames.RawValue, Flags.RawValue));
-
-    private readonly BsonSerializationInfo _currencyInfo = new(
-        ElementNames.Currency,
-        CurrencySerializer.Instance,
-        typeof(Currency));
-
-    private readonly BsonSerializationInfo _rawValueInfo = new(
-        ElementNames.RawValue,
-        BigIntegerSerializer.Instance,
-        typeof(BigInteger));
 
     public static FungibleAssetValue Deserialize(BsonDocument doc)
     {
@@ -48,34 +27,22 @@ public class FungibleAssetValueSerializer : StructSerializerBase<FungibleAssetVa
         BsonDeserializationContext context,
         BsonDeserializationArgs args)
     {
-        Currency currency = default;
-        BigInteger rawValue = default;
-        _helper.DeserializeMembers(context, (_, flag) =>
-        {
-            switch (flag)
-            {
-                case Flags.Currency:
-                    currency = (Currency)_currencyInfo.Serializer.Deserialize(context);
-                    break;
-                case Flags.RawValue:
-                    rawValue = (BigInteger)_rawValueInfo.Serializer.Deserialize(context);
-                    break;
-            }
-        });
-        return FungibleAssetValue.FromRawValue(currency, rawValue);
+        var doc = BsonDocumentSerializer.Instance.Deserialize(context, args);
+        return Deserialize(doc);
     }
 
-    public override void Serialize(
-        BsonSerializationContext context,
-        BsonSerializationArgs args,
-        FungibleAssetValue value)
-    {
-        var writer = context.Writer;
-        writer.WriteStartDocument();
-        writer.WriteName(ElementNames.Currency);
-        _currencyInfo.Serializer.Serialize(context, value.Currency);
-        writer.WriteName(ElementNames.RawValue);
-        _rawValueInfo.Serializer.Serialize(context, value.RawValue);
-        writer.WriteEndDocument();
-    }
+    // DO NOT OVERRIDE Serialize METHOD: Currently objects will be serialized to Json first.
+    // public override void Serialize(
+    //     BsonSerializationContext context,
+    //     BsonSerializationArgs args,
+    //     FungibleAssetValue value)
+    // {
+    //     var writer = context.Writer;
+    //     writer.WriteStartDocument();
+    //     writer.WriteName(ElementNames.Currency);
+    //     _currencyInfo.Serializer.Serialize(context, value.Currency);
+    //     writer.WriteName(ElementNames.RawValue);
+    //     _rawValueInfo.Serializer.Serialize(context, value.RawValue);
+    //     writer.WriteEndDocument();
+    // }
 }
