35cf0afac490bf0ea5c4eace66dc3652c7641cce
Kushagra Nigam <93257324+TheMarvelFan@users.noreply.github.com>
2024-10-02 14:00:36 +0530

    Fixed newline issues and namespace styling in 'BigIntegerJsonConverter.cs'

--- Mimir.MongoDB/Json/Converters/BigIntegerJsonConverter.cs
+++ Mimir.MongoDB/Json/Converters/BigIntegerJsonConverter.cs
@@ -2,38 +2,38 @@ using System.Numerics;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace Mimir.MongoDB.Json.Converters
+namespace Mimir.MongoDB.Json.Converters;
+
+public class BigIntegerJsonConverter : JsonConverter<BigInteger>
 {
-    public class BigIntegerJsonConverter : JsonConverter<BigInteger>
+    public override BigInteger Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        public override BigInteger Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        if (reader.TokenType == JsonTokenType.Null)
         {
-            if (reader.TokenType == JsonTokenType.Null)
-            {
-                throw new JsonException("Cannot convert null value to BigInteger.");
-            }
-
-            if (reader.TokenType == JsonTokenType.String)
-            {
-                var value = reader.GetString();
-                return BigInteger.Parse(value!);
-            }
-
-            if (reader.TokenType == JsonTokenType.Number)
-            {
-                if (reader.TryGetInt64(out long value))
-                {
-                    return new BigInteger(value);
-                }
-                throw new JsonException("Invalid number format for BigInteger.");
-            }
-
-            throw new JsonException($"Unexpected token type: {reader.TokenType}.");
+            throw new JsonException("Cannot convert null value to BigInteger.");
         }
 
-        public override void Write(Utf8JsonWriter writer, BigInteger value, JsonSerializerOptions options)
+        if (reader.TokenType == JsonTokenType.String)
         {
-            writer.WriteStringValue(value.ToString());
+            var value = reader.GetString();
+            return BigInteger.Parse(value!);
         }
+
+        if (reader.TokenType == JsonTokenType.Number)
+        {
+            if (reader.TryGetInt64(out long value))
+            {
+                return new BigInteger(value);
+            }
+            
+            throw new JsonException("Invalid number format for BigInteger.");
+        }
+
+        throw new JsonException($"Unexpected token type: {reader.TokenType}.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, BigInteger value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
     }
 }
