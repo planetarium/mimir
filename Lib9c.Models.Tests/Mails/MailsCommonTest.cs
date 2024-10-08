d5e4f8b6d0e5d253d318f7297900d6b91defa78d
Hyun Seungmin <boscohyun@gmail.com>
2024-10-08 12:14:47 +0900

    fix typo

--- Lib9c.Models.Tests/Mails/MailsCommonTest.cs
+++ Lib9c.Models.Tests/Mails/MailsCommonTest.cs
@@ -5,38 +5,38 @@ namespace Lib9c.Models.Tests.Mails;
 public class MailsCommonTest
 {
     [Fact]
-    public void Handle_All_Mail_Types_Of_Lib9c()
+    public void Handle_All_Relates_Types_Of_Mail()
     {
-        var targetMailType = typeof(Nekoyume.Model.Mail.Mail);
-        var targetMailNameTuples = targetMailType.Assembly.GetTypes()
-            .Where(t => t.IsAssignableTo(targetMailType))
+        var targetType = typeof(Nekoyume.Model.Mail.Mail);
+        var targetNameTuples = targetType.Assembly.GetTypes()
+            .Where(t => t.IsAssignableTo(targetType))
             .Select(t => (t.Name, t.FullName))
             .ToArray();
 
         // Prevent unexpected namespace. "Nekoyume.Model.Mail.{Mail}"
-        foreach (var (_, targetMailFullName) in targetMailNameTuples)
+        foreach (var (_, targetFullName) in targetNameTuples)
         {
-            Assert.NotNull(targetMailFullName);
-            Assert.StartsWith("Nekoyume.Model.Mail.", targetMailFullName);
+            Assert.NotNull(targetFullName);
+            Assert.StartsWith("Nekoyume.Model.Mail.", targetFullName);
         }
 
         var mailType = typeof(Mail);
-        var mailFullNameTuples = mailType.Assembly.GetTypes()
+        var mailNameTuples = mailType.Assembly.GetTypes()
             .Where(t => t.IsAssignableTo(mailType))
             .Select(t => (t.Name, t.FullName))
             .ToArray();
 
         // Prevent unexpected namespace. "Lib9c.Models.Mails.{Mail}"
-        foreach (var (_, mailFullName) in mailFullNameTuples)
+        foreach (var (_, mailFullName) in mailNameTuples)
         {
             Assert.NotNull(mailFullName);
             Assert.StartsWith("Lib9c.Models.Mails.", mailFullName);
         }
 
-        var mailNames = mailFullNameTuples.Select(e => e.Name).ToArray();
-        foreach (var (targetMailName, _) in targetMailNameTuples)
+        var mailNames = mailNameTuples.Select(e => e.Name).ToArray();
+        foreach (var (targetName, _) in targetNameTuples)
         {
-            Assert.Contains(targetMailName, mailNames);
+            Assert.Contains(targetName, mailNames);
         }
     }
 }
