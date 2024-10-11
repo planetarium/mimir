91e6800cc84995de71b5e6360ee04deba1c3aba4
Hyun Seungmin <boscohyun@gmail.com>
2024-10-11 14:21:45 +0900

    re-implement JoinArenaHandler

--- Mimir.Worker/ActionHandler/JoinArenaHandler.cs
+++ Mimir.Worker/ActionHandler/JoinArenaHandler.cs
@@ -1,74 +1,68 @@
-using Lib9c.Abstractions;
+using Bencodex.Types;
 using Lib9c.Models.States;
-using Libplanet.Action;
 using Libplanet.Crypto;
 using Mimir.Worker.CollectionUpdaters;
 using Mimir.Worker.Services;
 using MongoDB.Driver;
+using Nekoyume.Action;
 using Nekoyume.Model.EnumType;
 using Serilog;
 
 namespace Mimir.Worker.ActionHandler;
 
-public class JoinArenaHandler(IStateService stateService, MongoDbService store)
-    : BaseActionHandler(
+public class JoinArenaHandler(IStateService stateService, MongoDbService store) :
+    BaseActionHandler(
         stateService,
         store,
         "^join_arena[0-9]*$",
-        Log.ForContext<JoinArenaHandler>()
-    )
+        Log.ForContext<JoinArenaHandler>())
 {
+    private static readonly JoinArena Action = new();
+
     protected override async Task<bool> TryHandleAction(
         long blockIndex,
         Address signer,
-        IAction action,
+        IValue actionPlainValue,
+        string actionType,
+        IValue? actionPlainValueInternal,
         IClientSessionHandle? session = null,
-        CancellationToken stoppingToken = default
-    )
+        CancellationToken stoppingToken = default)
     {
-        if (action is not IJoinArenaV1 joinArena)
-        {
-            return false;
-        }
-
-        Logger.Information("Handle join_arena, address: {AvatarAddress}", joinArena.AvatarAddress);
-
+        Action.LoadPlainValue(actionPlainValue);
         await ItemSlotCollectionUpdater.UpdateAsync(
             StateService,
             Store,
             BattleType.Arena,
-            joinArena.AvatarAddress,
-            joinArena.Costumes,
-            joinArena.Equipments,
+            Action.avatarAddress,
+            Action.costumes,
+            Action.equipments,
             session,
             stoppingToken
         );
-
-        await ProcessArena(joinArena, session, stoppingToken);
-
+        await ProcessArena(Action, session, stoppingToken);
         return true;
     }
 
     private async Task ProcessArena(
-        IJoinArenaV1 joinArena,
+        JoinArena joinArena,
         IClientSessionHandle? session = null,
         CancellationToken stoppingToken = default
     )
     {
         var arenaScore = await StateGetter.GetArenaScoreAsync(
-            joinArena.AvatarAddress,
-            joinArena.ChampionshipId,
-            joinArena.Round,
+            joinArena.avatarAddress,
+            joinArena.championshipId,
+            joinArena.round,
             stoppingToken
         );
         var arenaInfo = await StateGetter.GetArenaInformationAsync(
-            joinArena.AvatarAddress,
-            joinArena.ChampionshipId,
-            joinArena.Round,
+            joinArena.avatarAddress,
+            joinArena.championshipId,
+            joinArena.round,
             stoppingToken
         );
         var avatarState = await StateGetter.GetAvatarState(
-            joinArena.AvatarAddress,
+            joinArena.avatarAddress,
             stoppingToken
         );
         var simpleAvatarState = SimplifiedAvatarState.FromAvatarState(avatarState);
@@ -77,9 +71,9 @@ public class JoinArenaHandler(IStateService stateService, MongoDbService store)
             simpleAvatarState,
             arenaScore,
             arenaInfo,
-            joinArena.AvatarAddress,
-            joinArena.ChampionshipId,
-            joinArena.Round,
+            joinArena.avatarAddress,
+            joinArena.championshipId,
+            joinArena.round,
             session,
             stoppingToken
         );
