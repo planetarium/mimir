17902b981a9e40aecadc9ee8174a045da521d106
Atralupus <me@atralupus.com>
2024-11-05 01:54:00 +0900

    Remove useless codes that relates to RuneSlotState

--- Mimir.MongoDB/Bson/RuneSlotDocument.cs
+++
@@ -1,8 +0,0 @@
-using Lib9c.Models.States;
-using Libplanet.Crypto;
-using MongoDB.Bson.Serialization.Attributes;
-
-namespace Mimir.MongoDB.Bson;
-
-[BsonIgnoreExtraElements]
-public record RuneSlotDocument(Address Address, RuneSlotState Object) : MimirBsonDocument(Address);
