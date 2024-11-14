d7154d406d8aa7406b2f83fefc544f7e1ad866c3
Hyun Seungmin <boscohyun@gmail.com>
2024-11-14 15:47:43 +0900

    clean up unit tests

---
+++ Mimir.Worker.Tests/Fixtures/States/StateReader.cs
@@ -0,0 +1,29 @@
+using Bencodex;
+using Bencodex.Types;
+using Libplanet.Common;
+
+namespace Mimir.Worker.Tests.Fixtures.States;
+
+public static class StateReader
+{
+    private static readonly string StatesFullPath = Path.GetFullPath(
+        Path.Combine(
+            Directory.GetCurrentDirectory(),
+            "../../../",
+            "Fixtures",
+            "States"));
+
+    private static readonly Codec Codec = new();
+
+    public static IValue ReadState(string fileName)
+    {
+        var file = GetFile(fileName);
+        return Codec.Decode(ByteUtil.ParseHex(file));
+    }
+
+    private static string GetFile(string fileName)
+    {
+        var path = Path.Combine(StatesFullPath, fileName);
+        return File.ReadAllText(path).Trim();
+    }
+}
