97b5ef535d0d5a178aa9c457992846f0d0341077
nakyoung <nacoon53@gmail.com>
2024-11-23 16:47:45 +0900

    fix DailyRewardTest

--- Mimir.Tests/QueryTests/DailyRewardQueryTest.cs
+++ Mimir.Tests/QueryTests/DailyRewardQueryTest.cs
@@ -1,24 +1,11 @@
-﻿using Libplanet.Crypto;
-using Libplanet.Types.Tx;
-using Mimir.MongoDB.Bson;
-using Mimir.MongoDB.Repositories;
-using Moq;
-
-namespace Mimir.Tests.QueryTests;
+﻿namespace Mimir.Tests.QueryTests;
 
 public class DailyRewardQueryTest
 {
     [Fact]
     public async Task GraphQL_Query_DailyRewardReceivedBlockIndex_Returns_CorrectValue()
     {
-        var mockAddress = new Address("0x0000000000000000000000000000000000000000");
-        var mockRepo = new Mock<IDailyRewardRepository>();
-
-        mockRepo
-            .Setup(repo => repo.GetByAddressAsync(mockAddress))
-            .ReturnsAsync(new DailyRewardDocument(0, mockAddress, 0));
-
-        var serviceProvider = TestServices.CreateServices(dailyRewardRepositoryMock: mockRepo);
+        var serviceProvider = TestServices.CreateServices();
 
         var query = """
                     query {
