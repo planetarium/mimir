fe24bf568572786a3ae63685a23c5f5b59ca299f
Mecit Sarıgüzel <mecitsariguzel@gmail.com>
2024-11-25 08:28:30 +0300

    add test for currency ticker string extension method 'ToCurrency()'

---
+++ Lib9c.GraphQL.Tests/Extensions/CurrencyTickerStringExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using Lib9c.GraphQL.Extensions;
+using Libplanet.Types.Assets;
+
+namespace Lib9c.GraphQL.Tests.Extensions;
+
+public class CurrencyTickerStringExtensionsTest
+{
+    public static IEnumerable<object[]> CurrencyData =>
+        new[]
+        {
+            Currencies.Mead,
+            Currencies.Crystal,
+            Currencies.Garage,
+            Currencies.StakeRune,
+            Currencies.DailyRewardRune,
+            Currencies.FreyaLiberationRune,
+            Currencies.FreyaBlessingRune,
+            Currencies.OdinWeaknessRune,
+            Currencies.OdinWisdomRune
+        }
+        .Select(currency => new object[] { currency });
+
+    [Theory]
+    [MemberData(nameof(CurrencyData))]
+    public void ToCurrency_ShouldConvertCorrectly(Currency currency)
+    {
+        var currencyTicker = currency.Ticker;
+
+        var result = currencyTicker.ToCurrency();
+
+        Assert.Equal(currency, result);
+    }
+}
