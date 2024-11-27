1f57d50b5dc19aa537ba84d6a570e4dfc9a60bf4
Atralupus <me@atralupus.com>
2024-11-27 13:26:24 +0900

    Fix IDailyRewardRepository di type

--- Mimir/Program.cs
+++ Mimir/Program.cs
@@ -44,7 +44,7 @@ builder.Services.AddSingleton<ArenaParticipantRepository>();
 builder.Services.AddSingleton<IAvatarRepository, AvatarRepository>();
 builder.Services.AddSingleton<BalanceRepository>();
 builder.Services.AddSingleton<CollectionRepository>();
-builder.Services.AddSingleton<DailyRewardRepository>();
+builder.Services.AddSingleton<IDailyRewardRepository, DailyRewardRepository>();
 builder.Services.AddSingleton<InventoryRepository>();
 builder.Services.AddSingleton<ItemSlotRepository>();
 builder.Services.AddSingleton<MetadataRepository>();
