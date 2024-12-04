271081a42b0c3e0d762d16636df8579103a108ed
pybsh <jnhyeon18@gmail.com>
2024-12-04 20:31:37 +0900

    Add integration test for Inventory Query

--- Mimir/GraphQL/Queries/Query.cs
+++ Mimir/GraphQL/Queries/Query.cs
@@ -106,7 +106,7 @@ public class Query
     /// </summary>
     /// <param name="address">The address of the avatar.</param>
     /// <returns>The inventory state for the specified avatar address.</returns>
-    public async Task<Inventory> GetInventoryAsync(Address address, [Service] InventoryRepository repo) =>
+    public async Task<Inventory> GetInventoryAsync(Address address, [Service] IInventoryRepository repo) =>
         (await repo.GetByAddressAsync(address)).Object;
 
     /// <summary>
