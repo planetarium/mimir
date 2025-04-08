f0dd939d57002e5c9bb530e703130ef928206ab9
Atralupus <me@atralupus.com>
2025-04-08 17:19:01 +0900

    Add WorldBossRewardMail

--- Lib9c.Models/Factories/MailFactory.cs
+++ Lib9c.Models/Factories/MailFactory.cs
@@ -42,6 +42,7 @@ public static class MailFactory
             "seller" => new SellerMail(d),
             nameof(UnloadFromMyGaragesRecipientMail) => new UnloadFromMyGaragesRecipientMail(d),
             nameof(PatrolRewardMail) => new PatrolRewardMail(d),
+            nameof(WorldBossRewardMail) => new WorldBossRewardMail(d),
             _ => throw new UnsupportedArgumentValueException<string>("typeId", typeId),
         };
     }
