c8633613118e11e6a84f77fea0f37008707938ca
Yang Chun Ung <qooraven@gmail.com>
2025-07-11 15:21:51 +0900

    Fix item deserialize

--- Lib9c.Models.Tests/Items/ConsumableTest.cs
+++ Lib9c.Models.Tests/Items/ConsumableTest.cs
@@ -1,6 +1,8 @@
 using Bencodex.Types;
-using Lib9c.Models.Items;
-using Nekoyume.TableData;
+using Nekoyume.Model.Elemental;
+using Nekoyume.Model.Item;
+using Nekoyume.Model.Stat;
+using Nekoyume.Model.State;
 
 namespace Lib9c.Models.Tests.Items;
 
@@ -9,26 +11,49 @@ public class ConsumableTest
     [Fact]
     public void Test()
     {
-        // Prepare target state
-        var row = new ConsumableItemSheet.Row();
-        row.Set(new[] { "0", "Food", "1", "Normal", string.Empty, "HP", "0", "ATK", "0" });
-        var target = new Nekoyume.Model.Item.Consumable(
-            row,
-            Guid.NewGuid(),
-            requiredBlockIndex: 0);
+        // Prepare legacy state
+        var legacyStatsMapDict = List.Empty
+            .Add(
+                Dictionary.Empty
+                    .Add("statType", StatType.HP.Serialize())
+                    .Add("value", 100m.Serialize())
+            )
+            .Add(
+                Dictionary.Empty
+                    .Add("statType", StatType.ATK.Serialize())
+                    .Add("value", 50m.Serialize())
+            );
+
+        var legacyDict = Dictionary.Empty
+            .Add((Text)"id", 0.Serialize())
+            .Add((Text)"grade", 1.Serialize())
+            .Add((Text)"item_type", ItemType.Consumable.Serialize())
+            .Add((Text)"item_sub_type", ItemSubType.Food.Serialize())
+            .Add((Text)"elemental_type", ElementalType.Normal.Serialize())
+            .Add((Text)"itemId", Guid.NewGuid().Serialize())
+            .Add((Text)"stats", legacyStatsMapDict)
+            .Add((Text)"skills", new List())
+            .Add((Text)"buffSkills", new List())
+            .Add((Text)"requiredBlockIndex", 1000L.Serialize());
+
+        var target = (Consumable)ItemFactory.Deserialize(legacyDict);
+        var hpStat = target.Stats.Single(i => i.StatType == StatType.HP);
+        Assert.Equal(100m, hpStat.BaseValue);
+        var atkStat = target.Stats.Single(i => i.StatType == StatType.ATK);
+        Assert.Equal(50m, atkStat.BaseValue);
 
         // serialize target state and deserialize as paired state
         var serialized = target.Serialize();
-        var paired = new Consumable(serialized);
-        // ...
+        var deserialized = new Models.Items.Consumable(serialized);
 
-        // serialize paired state and verify
-        var bencoded = paired.Bencoded;
-        Assert.Equal(serialized, bencoded);
-
-        // deserialize bencoded state as target2 and verify
-        var target2 = new Nekoyume.Model.Item.Consumable((Dictionary)bencoded);
-        var serialized2 = target2.Serialize();
-        Assert.Equal(serialized, serialized2);
+        // Check Deserialize from List
+        Assert.Equal(target.Id, deserialized.Id);
+        Assert.Equal(target.ItemType, deserialized.ItemType);
+        Assert.Equal(target.ItemSubType, deserialized.ItemSubType);
+        Assert.Equal(target.Grade, deserialized.Grade);
+        Assert.Equal(target.ElementalType, deserialized.ElementalType);
+        Assert.Equal(target.ItemId, deserialized.ItemId);
+        Assert.Equal(target.RequiredBlockIndex, deserialized.RequiredBlockIndex);
+        Assert.Equal(target.Stats.Count, deserialized.Stats.Count);
     }
 }
